44ec0c7695374ab85fe1be50daf4494055dc7281
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-06-08 16:44:29 +0800

    g

---
+++ src/EduTrack/ServiceLayer/Class1.cs
@@ -0,0 +1,21 @@
+﻿using DomainLayer.Models.CommonModel;
+using DomainLayer.Models.InstructorModel;
+using DomainLayer.StudentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+;
+
+namespace ServiceLayer
+{
+    public class Class1
+    {
+        public void nmnm(IStudentPersonalInfoModel studentPersonalInfo)
+        {
+            studentPersonalInfo.SrCode = "22-23892";
+            studentPersonalInfo.BirthDate = DateTime.Now;
+        }
+    }
+}
