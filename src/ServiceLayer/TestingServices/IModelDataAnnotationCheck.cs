d727386b7c023553e02214b5bc8559d9cc1bd0bb
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-06-10 17:29:48 +0800

    Added DomainModelServices.cs, ModelDataAnnotationCheck.cs, and IModelDataAnnotationCheck.cs

---
+++ src/ServiceLayer/TestingServices/IModelDataAnnotationCheck.cs
@@ -0,0 +1,7 @@
+﻿namespace ServiceLayer.TestingServices
+{
+    public interface IModelDataAnnotationCheck
+    {
+        void ValidateModelDataAnnotation<TDomainModel>(TDomainModel model) where TDomainModel : class;
+    }
+}
---
+++ src/ServiceLayer/TestingServices/ModelDataAnnotationCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.TestingServices
+{
+    class ModelDataAnnotationCheck : IModelDataAnnotationCheck
+    {
+        public void ValidateModelDataAnnotation<TDomainModel>(TDomainModel model) where TDomainModel : class
+        {
+            ICollection<ValidationResult> validationResultList = new List<ValidationResult>();
+
+            ValidationContext validationContext = new ValidationContext(model, null, null);
+
+            StringBuilder newErrorMessage = new StringBuilder();
+
+            if (!Validator.TryValidateObject(model, validationContext, validationResultList, validateAllProperties: true))
+            {
+                foreach (ValidationResult validationResult in validationResultList)
+                    newErrorMessage.AppendLine(validationResult.ErrorMessage);
+            }
+
+            if (validationResultList.Count > 0) throw new ArgumentException(newErrorMessage.ToString());
+        }
+    }
+}
