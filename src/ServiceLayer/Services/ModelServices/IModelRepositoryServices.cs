281403e3bfd6ad08cc6fadeb79f9a7b37fe1842a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-06-14 11:04:51 +0800

    Revert "Move DomainModelServices.cs to defintion directory"
    This reverts commit f83a2edd2356e8a34a90c7fe0c484c8aa8bb0c35.

---
+++ src/ServiceLayer/Services/ModelServices/IModelRepositoryServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services.ModelServices
+{
+    public interface IModelRepositoryServices
+    {
+        void Add<TDomainModel>(TDomainModel DomainDataModel) where TDomainModel : class;
+        void Update<TDomainModel>(TDomainModel DomainDataModel) where TDomainModel : class;
+        void Delete<TDomainModel>(TDomainModel DomainDataModel) where TDomainModel : class;
+        IEnumerable<TDomainModel> GetAll<TDomainModel>() where TDomainModel : class;
+        TDomainModel GetByID<TDomainModel>(string id) where TDomainModel : class;
+    }
+}
