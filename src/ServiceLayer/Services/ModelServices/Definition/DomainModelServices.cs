416668a445929ed430a43982fa802e465a0811b2
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-06-16 01:09:50 +0800

    Moved interface DomainModelRepository.cs to InfrastructureLayer.

--- src/ServiceLayer/Services/ModelServices/Definition/DomainModelServices.cs
+++ src/ServiceLayer/Services/ModelServices/Definition/DomainModelServices.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InfrastructureLayer.DataAccess.Repository.Model;
 using ServiceLayer.TestingServices;
+
 
 namespace ServiceLayer.Services.ModelServices
 {
