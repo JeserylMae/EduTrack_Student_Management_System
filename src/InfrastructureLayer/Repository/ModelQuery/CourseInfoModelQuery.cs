73fb9eabbf3b9577b17e41cc7c8ba888a5a585c5
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-06-17 14:42:40 +0800

    Added new classes.

---
+++ src/InfrastructureLayer/Repository/ModelQuery/CourseInfoModelQuery.cs
@@ -0,0 +1,44 @@
+﻿using InfrastructureLayer.Repository.Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InfrastructureLayer.Repository.ModelQuery
+{
+    internal class CourseInfoModelQuery
+    {
+        internal static string ModelQueryGenerator(AccessDefaultVariation.RequestType requestType,
+												   AccessDefaultVariation.RequestFrom requestFrom)
+        {
+			string modelQuery = string.Empty;
+
+            if(requestType == AccessDefaultVariation.RequestType.GetAll)
+			{
+				modelQuery = "SELECT * FROM CourseInfoTbl";
+			}
+			else if(requestType == AccessDefaultVariation.RequestType.GetById)
+			{
+				modelQuery = "SELECT * FROM CourseInfoTbl WHERE SrCode = @SrCode";
+			}
+			else if (requestType == AccessDefaultVariation.RequestType.Add)
+            {
+                modelQuery = "SELECT * FROM CourseInfoTbl";
+            }
+			else if(requestType == AccessDefaultVariation.RequestType.Update)
+			{
+				modelQuery = "UPDATE CourseInfoTbl "
+						   + "SET CourseName = @CourseName, NumberOfUnits = @NumberOfUnits "
+						   + "WHERE CourseCode = @CourseCode;";
+			}
+			else if(requestType == AccessDefaultVariation.RequestType.Delete)
+			{
+				modelQuery = "DELETE FROM CourseInfoTbl WHERE CourseCode = @CourseCode;";
+			}
+
+            return modelQuery;
+        }
+    }
+}
---
+++ src/InfrastructureLayer/Repository/ModelQuery/StudentAttendanceInfoModelQuery.cs
@@ -0,0 +1,43 @@
+﻿using InfrastructureLayer.Repository.Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Repository.ModelQuery
+{
+    internal class StudentAttendanceInfoModelQuery
+    {
+        internal static string ModelQueryGenerator(AccessDefaultVariation.RequestType requestType)
+        {
+            string modelQuery = string.Empty;
+
+            if (requestType == AccessDefaultVariation.RequestType.GetAllById)
+            {
+                modelQuery = "SELECT * FROM StudentAttendanceInfoTbl WHERE S";
+            }
+            else if (requestType == AccessDefaultVariation.RequestType.GetById)
+            {
+                modelQuery = "SELECT * FROM EdutrackUserTbl WHERE UserID = @UserID";
+            }
+            else if (requestType == AccessDefaultVariation.RequestType.Add)
+            {
+                modelQuery = "INSERT INTO EdutrackUserTbl(UserId, UserPassword, EmailAddress) "
+                           + "VALUES(@UserId, @UserPassword, @EmailAddress)";
+            }
+            else if (requestType == AccessDefaultVariation.RequestType.Update)
+            {
+                modelQuery = "UPDATE EdutrackUserTbl "
+                           + "SET UserPassword = @UserPassword, EmailAddress = @ EmailAddress "
+                           + "WHERE UserId = @UserId;";
+            }
+            else if (requestType == AccessDefaultVariation.RequestType.Delete)
+            {
+                modelQuery = "DELETE FROM EdutrackUserTbl WHERE UserId = @UserId;";
+            }
+
+            return modelQuery;
+        }
+    }
+}
