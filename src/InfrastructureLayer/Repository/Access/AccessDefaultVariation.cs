8432d4a85c52280c96b858654e3df454323f73bb
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-06-17 14:42:02 +0800

    Added new access variation.

---
+++ src/InfrastructureLayer/Repository/Access/AccessDefaultVariation.cs
@@ -0,0 +1,74 @@
+﻿using DomainLayer.Models.InformationModel.AcademicModel;
+using InfrastructureLayer.Repository.ModelQuery;
+using System.Collections.Generic;
+using System;
+using DomainLayer.Models.DerivedModel;
+
+namespace InfrastructureLayer.Repository.Access
+{
+    public static class AccessDefaultVariation
+    {
+        public static void InitializeQueryGenrator() 
+        {
+            _ModelDatabaseQueryGenerator = new Dictionary<string, Delegate>
+            {
+                { nameof(CourseInfoModel), new _GetModelQuery(CourseInfoModelQuery.ModelQueryGenerator) },
+                { nameof(EdutrackUserModel), new _GetModelQuery(EdutrackUserModelQuery.ModelQueryGenerator) }
+            };
+        }
+
+        // sample of invoking delegates
+        // if (delegateDictionary.TryGetValue("Add", out Delegate addDelegate))
+        // {
+        //    int result = (int)addDelegate.DynamicInvoke(10, 5);
+        // Console.WriteLine($"Addition Result: {result}");
+        // }
+
+        public enum TypeOfExistenceCheck
+        {
+            DoesExistInTheDB,
+            DoesNotExistInTheDB,
+        }
+
+        public enum RequestFrom
+        {
+            Admin,
+            Student,
+            Instructor
+        }
+
+        public enum RequestType
+        {
+            Add,
+            Update,
+            Delete,
+            GetAll,
+            GetById,
+            DeleteAll,
+            GetAllById,
+            ConfirmAdd,
+            ConfirmDelete
+        }
+
+        internal enum ModelValueAssignmentMethod
+        {
+            AssignCourseInfoModelValue,
+            AssignEdutrackUserInfoModelValue,
+            AssignStudentGradeInfoModelValue,
+            AssignStudentPersonalInfoModelValue,
+            AssignStudentAcademicInfoModelValue,
+            AssignStudentAttendanceInfoModelValue,
+            AssignInstructorPersonalInfoModelValue,
+            AssignInstructorAcademicInfoModelValue
+        }
+
+        internal static Dictionary<string, Func<string>> ModelDatabaseGenerator
+        {
+            get => ModelDatabaseGenerator;
+        }
+
+
+        private delegate string _GetModelQuery(RequestType requestType, RequestFrom requestFrom);
+        private static Dictionary<string, Delegate> _ModelDatabaseQueryGenerator;
+    }
+}
