9c0df0bb104876d8ab73c8e8c612241e8ad42f90
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-10 14:44:25 +0800

    Call connection string generator method.

--- src/PresentationLayer/Presenters/ServerInfoPresenter.cs
+++ src/PresentationLayer/Presenters/ServerInfoPresenter.cs
@@ -1,5 +1,6 @@
 ﻿using DomainLayer.WebModels;
 using PresentationLayer.Views;
+using ServiceLayer.Database;
 using System;
 using System.Windows.Forms;
 
@@ -12,31 +13,25 @@ namespace PresentationLayer.Presenters
             _serverInfoForm = serverInfoForm;
             _edutrackMainForm = edutrackMainForm;
 
-            _serverInfoForm.WindowExit          += ExitAppButton_Clicked;
-            _serverInfoForm.SubmittedServerInfo += ServerInfoButton_Clicked;
-            _serverInfoForm.SslCaButtonClicked += SslCaDialogButton_Clicked;
-            _serverInfoForm.SslKeyButtonClicked += SslKeyDialogButton_Clicked;
+            _serverInfoForm.WindowExit           += ExitAppButton_Clicked;
+            _serverInfoForm.SubmittedServerInfo  += ServerInfoButton_Clicked;
+            _serverInfoForm.SslCaButtonClicked   += SslCaDialogButton_Clicked;
+            _serverInfoForm.SslKeyButtonClicked  += SslKeyDialogButton_Clicked;
             _serverInfoForm.SslCertButtonClicked += SslCertDialogButton_Clicked;
         }
+
+        #region Button's Subcribers
 
         private void ExitAppButton_Clicked(object sender, EventArgs e) 
             => Application.Exit();
 
-        private void RunEdutrackForm()
-        {
-            _serverInfoForm.CloseForm();
-            _edutrackMainForm.FormWindowState = FormWindowState.Normal;
-        }
-
         private void ServerInfoButton_Clicked(object sender, EventArgs e)
         {
             ServerInfoModel serverInfo = new ServerInfoModel();
-            serverInfo.UserId      = _serverInfoForm.GetServerUserId;
-            serverInfo.Localhost   = _serverInfoForm.GetServerServerName;
-            serverInfo.Password    = _serverInfoForm.GetServerPassword;
-            serverInfo.SslCaPath   = _serverInfoForm.SslCaLabelText;
-            serverInfo.SslKeyPath  = _serverInfoForm.SslKeyLabelText;
-            serverInfo.SslCertPath = _serverInfoForm.SslCertLabelText;
+            AssignServerInfoFields(ref serverInfo);
+
+            string connectionString = DatabaseConnection.GenerateConnectionString(ref serverInfo);
+
 
             // Function that validates whether the connection string can
             // successfully send request to the web api.
@@ -64,7 +59,30 @@ namespace PresentationLayer.Presenters
             if (_serverInfoForm.ShowSslCertFileDialog() == DialogResult.OK)
                 _serverInfoForm.SslCertLabelText = _serverInfoForm.GetSslCertFileDialogFileName;
         }
-        
+
+        #endregion
+
+        #region Helper Methods
+
+        private void RunEdutrackForm()
+        {
+            _serverInfoForm.CloseForm();
+            _edutrackMainForm.FormWindowState = FormWindowState.Normal;
+        }
+
+        private void AssignServerInfoFields(ref ServerInfoModel serverInfo)
+        {
+            serverInfo.UserId = _serverInfoForm.GetServerUserId;
+            serverInfo.Localhost = _serverInfoForm.GetServerServerName;
+            serverInfo.Password = _serverInfoForm.GetServerPassword;
+            serverInfo.SslCaPath = _serverInfoForm.SslCaLabelText;
+            serverInfo.SslKeyPath = _serverInfoForm.SslKeyLabelText;
+            serverInfo.SslCertPath = _serverInfoForm.SslCertLabelText;
+        }
+
+        #endregion
+
+
         private IServerInfoForm _serverInfoForm;
         private IEdutrackMainForm _edutrackMainForm;
     }
