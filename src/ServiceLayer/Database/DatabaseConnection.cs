f52e9a5a2f17be6ddf85d6ddcf32d217380ad92a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-10 14:46:00 +0800

    Added new class contaning the methods necessary for db connection.

---
+++ src/ServiceLayer/Database/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿
+using DomainLayer.WebModels;
+
+
+namespace ServiceLayer.Database
+{
+    public static class DatabaseConnection
+    { 
+        public static string GenerateConnectionString(ref ServerInfoModel serverInfo)
+        {
+            return $"Server={serverInfo.Localhost};"
+                 + $"Database=edutrack_server;"
+                 + $"User={serverInfo.UserId};"
+                 + $"Password={serverInfo.Password};"
+                 + $"SslMode=Required;"
+                 + $"SslCa={serverInfo.SslCaPath};"
+                 + $"SslCert={serverInfo.SslCertPath};"
+                 + $"SslKey={serverInfo.SslKeyPath};";
+        }
+    }
+}
