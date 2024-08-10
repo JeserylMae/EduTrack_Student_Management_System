7e3a3b000f24b8184b94649e03ed78bac89ea137
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-10 14:13:58 +0800

    added properties to hold ssl connection info.

--- src/DomainLayer/WebModels/ServerInfoModel.cs
+++ src/DomainLayer/WebModels/ServerInfoModel.cs
@@ -3,8 +3,11 @@ namespace DomainLayer.WebModels
 {
     public class ServerInfoModel
     {
-        public string UserId { get; set; }
-        public string Localhost { get; set; }
-        public string Password { get; set; }
+        public string UserId      { get; set; }
+        public string Localhost   { get; set; }
+        public string Password    { get; set; }
+        public string SslCaPath   { get; set; }
+        public string SslKeyPath  { get; set; }
+        public string SslCertPath { get; set; }
     }
 }
