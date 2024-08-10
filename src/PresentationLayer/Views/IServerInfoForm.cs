0d01eae11fe7d9b819df1d448528b9644acfb23b
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-10 12:21:43 +0800

    added new eventHandlers, getters, and setters.

--- src/PresentationLayer/Views/IServerInfoForm.cs
+++ src/PresentationLayer/Views/IServerInfoForm.cs
@@ -7,12 +7,25 @@ namespace PresentationLayer.Views
     {
         void ShowForm();
         void CloseForm();
+        void ShowSslCaFileDialog();
+        void ShowSslKeyFileDialog();
+        void ShowSslCertFileDialog();
 
-        string GetServerUserId     { get; }
-        string GetServerPassword   { get; }
-        string GetServerServerName { get; }
+        string GetServerUserId      { get; }
+        string GetServerPassword    { get; }
+        string GetServerServerName  { get; }
+        string SetSslCaLabelText    { set; }
+        string SetSslKeyLabelText   { set; }
+        string SetSslCertLabelText  { set; }
+        string SetFileDialogInitDir { set; }
+        string GetSslCaFileDialogFileName   { get; }
+        string GetSslKeyFileDialogFileName  { get; }
+        string GetSslCertFileDialogFileName { get; }
 
         event EventHandler WindowExit;
+        event EventHandler SslCaButtonClicked;
+        event EventHandler SslKeyButtonClicked;
         event EventHandler SubmittedServerInfo;
+        event EventHandler SslCertButtonClicked;
     }
 }
--- src/PresentationLayer/Views/ServerInfoForm.Config.cs
+++ src/PresentationLayer/Views/ServerInfoForm.Config.cs
@@ -12,6 +12,14 @@ namespace PresentationLayer.Views
             if (submitButton != null) SubmitButtonCreated.TrySetResult(true);
         }
 
+        private void OnSSLButtonCreated(IconButton sslCaButton, IconButton sslKeyButton,
+                                        IconButton sslCertButton) 
+        {
+            if (sslCaButton != null) SslCaButtonCreated.TrySetResult(true); 
+            if (sslKeyButton != null) SslKeyButtonCreated.TrySetResult(true); 
+            if (sslCertButton != null) SslCertButtonCreated.TrySetResult(true); 
+        }
+
         private async Task InitializeExitAppButtonSubscriber()
         {
             await ExitAppButtonCreated.Task;
@@ -22,6 +30,18 @@ namespace PresentationLayer.Views
         {
             await SubmitButtonCreated.Task;
             SubmitServerInfoButton.Click += delegate { SubmittedServerInfo?.Invoke(this, EventArgs.Empty); };
+        }
+
+        private async Task InitializeSSLButtonSubscriber()
+        {
+            await SslCaButtonCreated.Task;
+            sslCaDialogButton.Click += delegate { SslCaButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await SslKeyButtonCreated.Task;
+            sslKeyDialogButton.Click += delegate { SslKeyButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await SslCertButtonCreated.Task;
+            sslCertDialogButton.Click += delegate { SslCertButtonClicked?.Invoke(this, EventArgs.Empty); };
         }
     }
 }
