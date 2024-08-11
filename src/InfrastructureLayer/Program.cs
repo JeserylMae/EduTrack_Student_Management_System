1bf2aec32310e620ed9493e6518d7d8620c9e043
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-11 16:24:15 +0800

    Enable deployment of the web api using the presentation layer.

--- src/InfrastructureLayer/Program.cs
+++ src/InfrastructureLayer/Program.cs
@@ -1,32 +1,54 @@
 
-using DomainLayer.DataModels;
+using InfrastructureLayer.Data;
+using InfrastructureLayer.Database;
+using System.Data;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddHttpClient();
-builder.Services.AddControllers();
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddTransient<UserModel>();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        string? connectionString = args.FirstOrDefault(arg => arg.StartsWith("ConnectionStrings="))?.Split("=")[1];
+
+        // Filter out the connection string argument to pass only the original arguments to the builder
+        var filteredArgs = args.Where(arg => !arg.StartsWith("ConnectionStrings=")).ToArray();
+
+        if (string.IsNullOrEmpty(connectionString))
+            throw new Exception("No connection string given.");
+
+        var builder = WebApplication.CreateBuilder(filteredArgs);
+        //this.InitializeAppBuilder();
+
+        // Add services to the container.
+
+        builder.Services.AddHttpClient();
+        builder.Services.AddControllers();
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddTransient<DatabaseContext>(sp => new DatabaseContext(connectionString));
+        builder.Services.AddTransient<IEndpointRepository, EndpointRepository>();
+        //builder.Services.AddTransient<IUserRepo, UserRepo>();
+        
+
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
