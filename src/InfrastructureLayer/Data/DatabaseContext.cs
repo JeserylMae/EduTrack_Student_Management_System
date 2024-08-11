1a56b96ee29b0f451326b733105cce2d77dfae9d
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-11 16:22:45 +0800

    Add Mysql connection builder.

---
+++ src/InfrastructureLayer/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace InfrastructureLayer.Data
+{
+    public class DatabaseContext
+    {
+        // private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DatabaseContext(string connectionString)
+        {
+            _connectionString = connectionString.Replace('@', '=');
+        }
+
+        // main ng web api
+        public IDbConnection CreateConnection() => new MySqlConnection(_connectionString);
+    }
+}
