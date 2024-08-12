278ac249e8c3e7fec461db1ffa748fa6e6a43ea5
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-12 19:09:02 +0800

    Temporarily removed the login page and server form for development of home page.

--- src/PresentationLayer/Presenters/EdutrackMainFormPresenter.cs
+++ src/PresentationLayer/Presenters/EdutrackMainFormPresenter.cs
@@ -18,7 +18,7 @@ namespace PresentationLayer.Presenters
             _edutrackMainForm.MouseMoved      += EdutrackMainForm_MouseMove;
             _edutrackMainForm.MousePressed    += EdutrackMainForm_MouseDown;
 
-            _edutrackMainForm.UserControlPage = new LogInPage(edutrackMainForm);
+            _edutrackMainForm.UserControlPage = new HomePage();
         }
 
         private void MinimizeAppButton_Click(object sender, EventArgs e)
