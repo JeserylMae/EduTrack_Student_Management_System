69f1fe68b8bc0d8ee7aeda040e687f77709d3375
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-12 12:14:56 +0800

    Added GetUserById web api request.

---
+++ src/ServiceLayer/Database/UserServices.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using DomainLayer.DataModels;
+
+namespace ServiceLayer.Database
+{
+    public class UserServices
+    {
+        public UserServices()
+        {
+            _webAddress = $"{DatabaseConnection.GetWebAddress()}/User";
+        }
+
+        public async Task<UserModel> GetUserByID(string UserId)
+        {
+            string request = $"{_webAddress}/GetById?UserId={UserId}";
+            Uri endpoint = new Uri(request);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<UserModel>(jsonResponse);
+                }
+                else { return null; }
+            }
+        }
+
+        private string _webAddress;
+    }
+}
