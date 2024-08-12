4f47d515943175d252e11d18e1e15f040becce59
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-12 09:48:53 +0800

    Added password character.

--- src/PresentationLayer/Views/ServerInfoForm.Designer.cs
+++ src/PresentationLayer/Views/ServerInfoForm.Designer.cs
@@ -301,6 +301,7 @@
             this.PasswordTextbox.Font = new System.Drawing.Font("Candara", 13F);
             this.PasswordTextbox.Location = new System.Drawing.Point(139, 163);
             this.PasswordTextbox.Name = "PasswordTextbox";
+            this.PasswordTextbox.PasswordChar = '●';
             this.PasswordTextbox.Size = new System.Drawing.Size(268, 22);
             this.PasswordTextbox.TabIndex = 10;
             // 
