be615e6cd463e44f1d50f38e74a233d5ea4cda79
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-15 12:45:56 +0800

    Modified for testing.

--- src/PresentationLayer/Views/EdutrackMainForm.cs
+++ src/PresentationLayer/Views/EdutrackMainForm.cs
@@ -18,9 +18,9 @@ namespace PresentationLayer.Views
             _ = AppMainPanelEventSubscriber();
             _ = InitializeTopBarButtonSubscribers();
 
-            MaximizeAppbutton.Enabled = false;
-            WindowState = FormWindowState.Minimized;
-            RunServerForm();
+            MaximizeAppbutton.Enabled = true;
+            WindowState = FormWindowState.Normal;
+            // RunServerForm();
         }
 
         public UserControl UserControlPage
