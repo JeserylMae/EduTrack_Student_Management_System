a2b91e6b20008f44afea294d26587c7554d0836f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-21 22:00:37 +0800

    Added new class which collects all the parameters for inserting new student.

---
+++ src/DomainLayer/DataModels/PersonalInfoModel.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace DomainLayer.DataModels
+{
+    public class PersonalInfoModel<TModel>
+    {
+
+        public TModel InfoModel       { get; set; }
+        public string DefaultPassword { get; set; }
+        public string Position        { get; set; }
+        public string StudentCode     { get; set; }
+        public string GuardianCode    { get; set; }
+    }
+}
