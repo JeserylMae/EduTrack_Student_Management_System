2527fd31b142395451af211ab5b9be364bba0afd
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-08-22 18:59:19 +0800

    update..

--- src/PresentationLayer/Program.cs
+++ src/PresentationLayer/Program.cs
@@ -4,6 +4,8 @@ using System.Windows.Forms;
 using PresentationLayer.Views;
 using PresentationLayer.Presenters;
 using ServiceLayer.ConsoleServices;
+using ServiceLayer.Database;
+using System.Reflection;
 
 ///
 /// CREATE A METHOD WHICH AUTOMATICALLY ADDS THE secrets.json file 
@@ -40,3 +42,13 @@ namespace PresentationLayer
         }
     }
 }
+
+
+//This exception was originally thrown at this call stack:
+//    ServiceLayer.Database.StudentPersonalInfoServices.HasNullOrEmptyString<TModel>(TModel) in StudentPersonalInfoServices.cs
+//    ServiceLayer.Database.StudentPersonalInfoServices.HasNullOrEmptyString<TModel>(TModel) in StudentPersonalInfoServices.cs
+//    ServiceLayer.Database.StudentPersonalInfoServices.HasNullOrEmptyString<TModel>(TModel) in StudentPersonalInfoServices.cs
+//    ServiceLayer.Database.StudentPersonalInfoServices.ValidateParameter(DomainLayer.DataModels.PersonalInfoModel<DomainLayer.DataModels.StudentPersonalInfoModel>) in StudentPersonalInfoServices.cs
+//    System.Runtime.CompilerServices.AsyncMethodBuilderCore.ThrowAsync.AnonymousMethod__6_0(object)
+
+
