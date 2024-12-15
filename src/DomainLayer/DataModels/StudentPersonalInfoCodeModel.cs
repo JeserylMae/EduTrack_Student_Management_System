a30c9421ce6d3d22e0acaaefe3e0c39df90ff7f6
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-15 18:10:33 +0800

    Created new model for student personal info table id. FINISHED

---
+++ src/DomainLayer/DataModels/StudentPersonalInfoCodeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels
+{
+    public class StudentPersonalInfoCodeModel
+    {
+        public string SrCode              { get; set; }
+        public string StudentNameCode     { get; set; }
+        public string StudentAddressCode  { get; set; }
+        public string GuardianNameCode    { get; set; }
+        public string GuardianAddressCode { get; set; }
+    }
+}
