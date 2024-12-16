c76d0224a28072d7215c556a5e245447080cf661
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-16 13:18:21 +0800

    Added new triggers and event subscriber to reduce tight coupling. FINISHED

--- src/PresentationLayer/Presenters/GeneralPresenter.cs
+++ src/PresentationLayer/Presenters/GeneralPresenter.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
 
 namespace PresentationLayer.Presenters
 {
     internal static class GeneralPresenter
     {
-        public static event EventHandler WindowExitSubscriber;
+        public static UserControl NewWindowControl;
 
-        public static void TriggerAppExit(object sender, EventArgs e)
-        {
-            WindowExitSubscriber.Invoke(sender, e);
-        }
+        public static event EventHandler WindowExitSubscriber;
+        public static event EventHandler WindowOpenControlSubscriber;
+
+        public static void TriggerAppExit(object sender, EventArgs e) 
+            => WindowExitSubscriber.Invoke(sender, e);
+
+        public static void TriggerWindowControlChange(object sender, EventArgs e) 
+            => WindowOpenControlSubscriber.Invoke(sender, e);
     }
 }
