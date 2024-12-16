7103b8bd810964e8879783104e303d6ae6371855
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-16 12:59:33 +0800

    Created a general presenter where similar event subscriber are placed.

---
+++ src/PresentationLayer/Presenters/GeneralPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace PresentationLayer.Presenters
+{
+    internal static class GeneralPresenter
+    {
+        public static event EventHandler WindowExitSubscriber;
+
+        public static void TriggerAppExit(object sender, EventArgs e)
+        {
+            WindowExitSubscriber.Invoke(sender, e);
+        }
+    }
+}
