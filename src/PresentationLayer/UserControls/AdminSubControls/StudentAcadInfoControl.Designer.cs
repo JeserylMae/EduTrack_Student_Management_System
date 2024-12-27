cbbf17db5ec1ff27ec0e975797276c92845dbc6e
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-27 19:00:06 +0800

    Added new control, Student Academic info form. IN PROGRESS

---
+++ src/PresentationLayer/UserControls/AdminSubControls/StudentAcadInfoControl.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace PresentationLayer.UserControls.AdminSubControls
+{
+    partial class StudentAcadInfoControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PageIndicatorLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SrCodeLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.FullNameLabel = new System.Windows.Forms.Label();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.FirstNameLabel = new System.Windows.Forms.Label();
+            this.MiddleNameLabel = new System.Windows.Forms.Label();
+            this.MiddleNameTextBox = new System.Windows.Forms.TextBox();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new FontAwesome.Sharp.IconButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PageIndicatorLabel
+            // 
+            this.PageIndicatorLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PageIndicatorLabel.AutoSize = true;
+            this.PageIndicatorLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PageIndicatorLabel.Font = new System.Drawing.Font("Candara", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PageIndicatorLabel.ForeColor = System.Drawing.Color.Black;
+            this.PageIndicatorLabel.Location = new System.Drawing.Point(47, 42);
+            this.PageIndicatorLabel.Name = "PageIndicatorLabel";
+            this.PageIndicatorLabel.Size = new System.Drawing.Size(426, 33);
+            this.PageIndicatorLabel.TabIndex = 142;
+            this.PageIndicatorLabel.Text = "Student Personal Information Form";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(28, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(443, 1);
+            this.label1.TabIndex = 143;
+            // 
+            // SrCodeLabel
+            // 
+            this.SrCodeLabel.AutoSize = true;
+            this.SrCodeLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(207)))), ((int)(((byte)(212)))));
+            this.SrCodeLabel.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SrCodeLabel.ForeColor = System.Drawing.Color.Black;
+            this.SrCodeLabel.Location = new System.Drawing.Point(62, 137);
+            this.SrCodeLabel.Name = "SrCodeLabel";
+            this.SrCodeLabel.Size = new System.Drawing.Size(73, 19);
+            this.SrCodeLabel.TabIndex = 145;
+            this.SrCodeLabel.Text = "SR-CODE:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(207)))), ((int)(((byte)(212)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Candara", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(154, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(306, 43);
+            this.textBox1.TabIndex = 146;
+            // 
+            // FullNameLabel
+            // 
+            this.FullNameLabel.AutoSize = true;
+            this.FullNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.FullNameLabel.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FullNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.FullNameLabel.Location = new System.Drawing.Point(59, 197);
+            this.FullNameLabel.Name = "FullNameLabel";
+            this.FullNameLabel.Size = new System.Drawing.Size(147, 18);
+            this.FullNameLabel.TabIndex = 147;
+            this.FullNameLabel.Text = "STUDENT FULL NAME";
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.LastNameLabel.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.LastNameLabel.Location = new System.Drawing.Point(59, 224);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(77, 18);
+            this.LastNameLabel.TabIndex = 148;
+            this.LastNameLabel.Text = "Last Name:";
+            // 
+            // FirstNameLabel
+            // 
+            this.FirstNameLabel.AutoSize = true;
+            this.FirstNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.FirstNameLabel.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.FirstNameLabel.Location = new System.Drawing.Point(59, 248);
+            this.FirstNameLabel.Name = "FirstNameLabel";
+            this.FirstNameLabel.Size = new System.Drawing.Size(80, 18);
+            this.FirstNameLabel.TabIndex = 149;
+            this.FirstNameLabel.Text = "First Name:";
+            // 
+            // MiddleNameLabel
+            // 
+            this.MiddleNameLabel.AutoSize = true;
+            this.MiddleNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.MiddleNameLabel.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MiddleNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.MiddleNameLabel.Location = new System.Drawing.Point(59, 273);
+            this.MiddleNameLabel.Name = "MiddleNameLabel";
+            this.MiddleNameLabel.Size = new System.Drawing.Size(97, 18);
+            this.MiddleNameLabel.TabIndex = 150;
+            this.MiddleNameLabel.Text = "Middle Name:";
+            // 
+            // MiddleNameTextBox
+            // 
+            this.MiddleNameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.MiddleNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MiddleNameTextBox.ForeColor = System.Drawing.Color.Black;
+            this.MiddleNameTextBox.Location = new System.Drawing.Point(177, 273);
+            this.MiddleNameTextBox.Name = "MiddleNameTextBox";
+            this.MiddleNameTextBox.Size = new System.Drawing.Size(148, 19);
+            this.MiddleNameTextBox.TabIndex = 151;
+            this.MiddleNameTextBox.Text = "Navarro";
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.FirstNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.FirstNameTextBox.ForeColor = System.Drawing.Color.Black;
+            this.FirstNameTextBox.Location = new System.Drawing.Point(177, 248);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(148, 19);
+            this.FirstNameTextBox.TabIndex = 152;
+            this.FirstNameTextBox.Text = "Navarro";
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.LastNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LastNameTextBox.ForeColor = System.Drawing.Color.Black;
+            this.LastNameTextBox.Location = new System.Drawing.Point(177, 224);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(148, 19);
+            this.LastNameTextBox.TabIndex = 153;
+            this.LastNameTextBox.Text = "Navarro";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = global::PresentationLayer.Properties.Resources.empty_profile_male1;
+            this.pictureBox2.Location = new System.Drawing.Point(371, 192);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 154;
+            this.pictureBox2.TabStop = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.IconChar = FontAwesome.Sharp.IconChar.X;
+            this.CloseButton.IconColor = System.Drawing.Color.Black;
+            this.CloseButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.CloseButton.IconSize = 24;
+            this.CloseButton.Location = new System.Drawing.Point(443, 13);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(32, 32);
+            this.CloseButton.TabIndex = 141;
+            this.CloseButton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::PresentationLayer.Properties.Resources.gray_label_highlighter;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 144;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(139)))), ((int)(((byte)(172)))));
+            this.label2.Location = new System.Drawing.Point(366, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 110);
+            this.label2.TabIndex = 155;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 156;
+            this.label3.Text = "label3";
+            // 
+            // StudentAcadInfoControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(0, 700);
+            this.BackgroundImage = global::PresentationLayer.Properties.Resources.panel_border_with_thick_bottom;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Controls.Add(this.MiddleNameTextBox);
+            this.Controls.Add(this.MiddleNameLabel);
+            this.Controls.Add(this.FirstNameLabel);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.FullNameLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SrCodeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PageIndicatorLabel);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Candara", 11.25F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "StudentAcadInfoControl";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Size = new System.Drawing.Size(518, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton CloseButton;
+        private System.Windows.Forms.Label PageIndicatorLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label SrCodeLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label FullNameLabel;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.Label FirstNameLabel;
+        private System.Windows.Forms.Label MiddleNameLabel;
+        private System.Windows.Forms.TextBox MiddleNameTextBox;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
