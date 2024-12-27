c3d22c623e3ce916a88e5ba92c2534f79cdbfd3f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-27 17:09:01 +0800

    Added method subscribers to button events. IN PROGRESS

--- src/PresentationLayer/Presenters/Admin/StudentModifyAcadInfoPresenter.cs
+++ src/PresentationLayer/Presenters/Admin/StudentModifyAcadInfoPresenter.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PresentationLayer.Presenters.General;
+using PresentationLayer.UserControls.HomeSubControls;
+using PresentationLayer.UserControls.MainControls;
 
 namespace PresentationLayer.Presenters.Admin
 {
@@ -16,7 +19,58 @@ namespace PresentationLayer.Presenters.Admin
         {
             _studentAcadInfoControl = studentAcadInfoControl;
 
-            _studentAcadInfoControl.ControlLoad += StudentAcadControl_Load;
+            _studentAcadInfoControl.ControlLoad                         += StudentAcadControl_Load;
+            _studentAcadInfoControl.CloseEditorButtonClicked            += CloseEditorButton_Clicked;
+            _studentAcadInfoControl.ExitButtonClicked                   += GeneralPresenter.TriggerAppExit;
+            _studentAcadInfoControl.FileDropDownButtonClicked           += FileDropDownButton_Clicked;
+            _studentAcadInfoControl.InstructorAcadInfoButtonClicked     += InstructorAcadInfoButton_Clicked;
+            _studentAcadInfoControl.StudentPersonalInfoButtonClicked    += StudentPersonalInfoButton_Clicked;
+            _studentAcadInfoControl.InstructorPersonalInfoButtonClicked += InstructorPersonalInfoButton_Clicked;
+        }
+
+
+        private void CloseEditorButton_Clicked(object sender, EventArgs e)
+        {
+            IHomePage homePage = new HomePage();
+            new HomePagePresenter(homePage);
+
+            IAdminHomeRightControl adminControl = new AdminHomeRightControl();
+            homePage.RightUserControlPage = (UserControl)adminControl;
+            new AdminHomeRightPresenter(adminControl);
+
+            GeneralPresenter.NewWindowControl = (UserControl)homePage;
+            GeneralPresenter.TriggerWindowControlChange(_studentAcadInfoControl, e);
+
+            _studentAcadInfoControl.DisposeControl();
+        }
+
+        private void StudentPersonalInfoButton_Clicked(object sender, EventArgs e)
+        {
+            IStudentModifyPersonalInfoControl studentControl = new StudentModifyPersonalInfoControl();
+            new StudentModifyPersonalInfoPresenter(studentControl);
+
+            GeneralPresenter.NewWindowControl = (UserControl)studentControl;
+            GeneralPresenter.TriggerWindowControlChange(_studentAcadInfoControl, e);
+
+            _studentAcadInfoControl.DisposeControl();
+        }
+
+        private void InstructorPersonalInfoButton_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InstructorAcadInfoButton_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FileDropDownButton_Clicked(object sender, EventArgs e)
+        {
+            if (_studentAcadInfoControl.AccessFileDropDownLayout.Visible)
+                _studentAcadInfoControl.AccessFileDropDownLayout.Visible = false;
+            else
+                _studentAcadInfoControl.AccessFileDropDownLayout.Visible = true;
         }
 
         public void StudentAcadControl_Load(object sender, EventArgs e)
