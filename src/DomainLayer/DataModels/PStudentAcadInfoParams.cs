ce3f40d4b1710fe5f4f15d509629b74fc2bab732
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-28 18:06:09 +0800

    Created new model for parameters of GetByParams and Delete in student acad info repo. FINISHED

---
+++ src/DomainLayer/DataModels/PStudentAcadInfoParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels
+{
+    public class PStudentAcadInfoParams
+    {
+        public string SrCode {  get; set; }
+        public string AcademicYear { get; set; }
+        public string YearLevel { get; set; }
+        public string Semester {  get; set; }
+    }
+}
