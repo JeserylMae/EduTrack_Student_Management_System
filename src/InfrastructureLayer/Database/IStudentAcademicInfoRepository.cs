ab283530ef9c90bc6f95234baae9b5a29036f648
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-28 18:07:08 +0800

    Created database api. IN PROGRESS

--- src/InfrastructureLayer/Database/IStudentAcademicInfoRepository.cs
+++ src/InfrastructureLayer/Database/IStudentAcademicInfoRepository.cs
@@ -5,5 +5,9 @@ namespace InfrastructureLayer.Database
     public interface IStudentAcademicInfoRepository
     {
         Task<List<RStudentAcademicInfoModel>> GetAll();
+        Task<int> Update(RStudentAcademicInfoModel studentModel);
+        Task<int> InsertNew(RStudentAcademicInfoModel studentModel);
+        Task<int> DeleteStudent(PStudentAcadInfoParams paramsModel);
+        Task<RStudentAcademicInfoModel> GetByParams(PStudentAcadInfoParams paramsModel);
     }
 }
--- src/InfrastructureLayer/Database/MStudentAcademicInfoRepository.cs
+++ src/InfrastructureLayer/Database/MStudentAcademicInfoRepository.cs
@@ -29,6 +29,87 @@ namespace InfrastructureLayer.Database
             }
         }
 
+        public Task<int> DeleteStudent(PStudentAcadInfoParams paramsModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<RStudentAcademicInfoModel> GetByParams(PStudentAcadInfoParams paramsModel)
+        {
+            StudentAcadParams parameterType = HandleParameter(paramsModel);
+            string procedure = _studentQuery.spGetById(parameterType);
+
+
+            using (IDbConnection connection = _databaseContext.CreateConnection())
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                AddDynamicParameters(ref parameters, parameterType, paramsModel);
+
+                var result = await connection.QuerySingleOrDefaultAsync<RStudentAcademicInfoModel>(
+                                procedure, parameters,
+                                commandType: CommandType.Text
+                );
+                return result;
+            }
+        }
+
+        public Task<int> Update(RStudentAcademicInfoModel studentModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertNew(RStudentAcademicInfoModel studentModel)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private StudentAcadParams HandleParameter(PStudentAcadInfoParams model)
+        {
+            if (!String.IsNullOrEmpty(model.Semester))
+            {
+                return StudentAcadParams.SrCodeAndAcadYearAndYearLevelAndSemester;
+            }
+            else if (!String.IsNullOrEmpty(model.Semester))
+            {
+                return StudentAcadParams.SrCodeAndAcadYearAndYearLevel;
+            }
+            else if (!String.IsNullOrEmpty(model.AcademicYear))
+            {
+                return StudentAcadParams.SrCodeAndAcadYear;
+            }
+            else if (!String.IsNullOrEmpty(model.SrCode))
+            {
+                return StudentAcadParams.SrCode;
+            }
+            return StudentAcadParams.None;
+        }
+
+        private void AddDynamicParameters(ref DynamicParameters parameters, StudentAcadParams parameterType,
+                                          PStudentAcadInfoParams studentModel)
+        {
+            switch (parameterType)
+            {
+                case StudentAcadParams.SrCode:
+                    parameters.Add("@p_SrCode", studentModel.SrCode);
+                    break;
+                case StudentAcadParams.SrCodeAndAcadYear:
+                    parameters.Add("@p_SrCode", studentModel.SrCode);
+                    parameters.Add("@p_AcademicYear", studentModel.AcademicYear);
+                    break;
+                case StudentAcadParams.SrCodeAndAcadYearAndYearLevel:
+                    parameters.Add("@p_SrCode", studentModel.SrCode);
+                    parameters.Add("@p_AcademicYear", studentModel.AcademicYear);
+                    parameters.Add("@p_YearLevel", studentModel.YearLevel);
+                    break;
+                case StudentAcadParams.SrCodeAndAcadYearAndYearLevelAndSemester:
+                    parameters.Add("@p_SrCode", studentModel.SrCode);
+                    parameters.Add("@p_AcademicYear", studentModel.AcademicYear);
+                    parameters.Add("@p_YearLevel", studentModel.YearLevel);
+                    parameters.Add("@p_Semester", studentModel.Semester);
+                    break;
+            }
+        }
 
         private DatabaseContext _databaseContext;
         private StudentAcadInfoQuery _studentQuery;
