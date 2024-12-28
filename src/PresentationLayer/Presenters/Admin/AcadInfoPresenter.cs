575cab17555a92cf4e6d7e3688c12f20f1dc4c6b
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-28 15:45:56 +0800

    Initialized presenter for Student Acad Info Control. FINISHED

---
+++ src/PresentationLayer/Presenters/Admin/AcadInfoPresenter.cs
@@ -0,0 +1,19 @@
+﻿using PresentationLayer.UserControls.AdminSubControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Presenters.Admin
+{
+    internal class AcadInfoPresenter
+    {
+        public AcadInfoPresenter(IStudentAcadInfoControl studentAcadInfoControl)
+        {
+            _studentAcadInfoControl = studentAcadInfoControl;
+        }
+
+        private IStudentAcadInfoControl _studentAcadInfoControl;
+    }
+}
