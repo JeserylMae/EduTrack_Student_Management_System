3866d5cf8438af0e5fa47ae1639c57aedfe75c61
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-29 19:16:47 +0800

    Added new method subscriber. FINISHED

--- src/PresentationLayer/Presenters/Admin/AcadInfoPresenter.cs
+++ src/PresentationLayer/Presenters/Admin/AcadInfoPresenter.cs
@@ -1,4 +1,6 @@
-﻿using PresentationLayer.UserControls.AdminSubControls;
+﻿using PresentationLayer.Presenters.Enumerations;
+using PresentationLayer.Presenters.General;
+using PresentationLayer.UserControls.AdminSubControls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +14,32 @@ namespace PresentationLayer.Presenters.Admin
         public AcadInfoPresenter(IStudentAcadInfoControl studentAcadInfoControl)
         {
             _studentAcadInfoControl = studentAcadInfoControl;
+
+            _studentAcadInfoControl.ControlLoad += StudentAcadInfoControl_Load;
+            _studentAcadInfoControl.CloseButtonClicked += CloseButton_Clicked;
         }
+
+        private void CloseButton_Clicked(object sender, EventArgs e)
+        {
+            _studentAcadInfoControl.CurrentControl.Dispose();
+        }
+
+        private void StudentAcadInfoControl_Load(object sender, EventArgs e)
+        {
+            if (_studentAcadInfoControl.CurrentRequestType == FormRequestType.UPDATE)
+                SetNameTextBoxToReadOnly();
+
+        }
+
+
+        #region Helpers
+        private void SetNameTextBoxToReadOnly()
+        {
+            _studentAcadInfoControl.AccessLastNameTextBox.ReadOnly = true;
+            _studentAcadInfoControl.AccessFirstNameTextBox.ReadOnly = true;
+            _studentAcadInfoControl.AccessMiddleNameTextBox.ReadOnly = true;
+        }
+        #endregion
 
         private IStudentAcadInfoControl _studentAcadInfoControl;
     }
