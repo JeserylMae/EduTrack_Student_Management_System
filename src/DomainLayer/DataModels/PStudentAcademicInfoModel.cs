a77ffa3ec0286a3852f7e51faaf34e0f0f3b9418
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-29 14:14:32 +0800

    Added new model for student academic info. FINISHED

---
+++ src/DomainLayer/DataModels/PStudentAcademicInfoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels
+{
+    public class PStudentAcademicInfoModel<TModel>
+    {
+        public string SrCode        { get; set; }  
+        public string YearLevel     { get; set; }
+        public string Semester      { get; set; }
+        public string Section       { get; set; }
+        public string AcademicYear  { get; set; }
+        public string Program       { get; set; }
+        public TModel StudentName   { get; set; }
+    }
+}
