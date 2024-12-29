e542ae90dc4c7db8f649b4e888a92cd64e72e070
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-29 10:39:22 +0800

    Added controllers for insertnew, delete, and update. FINISHED FOR TESTING

--- src/InfrastructureLayer/Controllers/StudentAcademicInfoController.cs
+++ src/InfrastructureLayer/Controllers/StudentAcademicInfoController.cs
@@ -37,6 +37,36 @@ namespace InfrastructureLayer.Controllers
             return NotFound(new { Message = $"Student with Sr-Code {studentModel.SrCode} not found." });
         }
 
+        [HttpPost("InsertNew")]
+        public async Task<IActionResult> InsertNew(RStudentAcademicInfoModel studentModel)
+        {
+            int response = await _studentAcademicRepository.InsertNew(studentModel);
+
+            if (response != 0) return Ok(response);
+            return BadRequest(new { Message = $"An error occured! Failed to add student with Sr-Code {studentModel.SrCode}." });
+        }
+
+        [HttpPatch("Update")]
+        public async Task<IActionResult> Update(RStudentAcademicInfoModel studentModel)
+        {
+            int response = await _studentAcademicRepository.Update(studentModel);
+
+            if (response != 0) return Ok(response);
+            return BadRequest(new { Message = $"An error occured! Failed to update student with Sr-Code {studentModel.SrCode}" });
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete(PStudentAcadInfoParams? studentModel)
+        {
+            if (studentModel == null)
+                return BadRequest(new { Message = "At least one parameter must be filled." });
+
+            int response = await _studentAcademicRepository.DeleteStudent(studentModel);
+
+            if (response != null) return Ok(response);
+            return NotFound(new { Message = $"Failed to delete student with Sr-Code {studentModel.SrCode}." });
+        }
+
 
         private IStudentAcademicInfoRepository _studentAcademicRepository;
     }
