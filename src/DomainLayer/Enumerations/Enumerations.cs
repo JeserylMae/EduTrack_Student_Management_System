692c765d675b3a3b0aff1d1ff7c78de2fc80800a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-29 19:13:03 +0800

    Created a new file containing all the enums. FINISHED

---
+++ src/DomainLayer/Enumerations/Enumerations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Presenters.Enumerations
+{
+    public enum FormRequestType
+    {
+        ADD, UPDATE
+    }
+
+    public enum StudentAcadParams
+    {
+        None,
+        SrCode,
+        SrCodeAndAcadYear,
+        SrCodeAndAcadYearAndYearLevel,
+        SrCodeAndAcadYearAndYearLevelAndSemester
+    }
+}
