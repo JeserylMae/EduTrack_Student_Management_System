4a192e3d4f708ab463b9d1f790dde25794e94d6b
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-29 18:18:58 +0800

    Created new api request methods for student acad info. FINISHED

--- src/ServiceLayer/Database/StudentAcademicInfoServices.cs
+++ src/ServiceLayer/Database/StudentAcademicInfoServices.cs
@@ -2,10 +2,14 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 
 namespace ServiceLayer.Database
 {
@@ -17,7 +21,7 @@ namespace ServiceLayer.Database
         }
 
 
-        public List<PStudentAcademicInfoModel<PNameModel>> GetAll()
+        public async Task<List<PStudentAcademicInfoModel<PNameModel>>> GetAll()
         {
             string request = $"{_webAddress}/GetAll";
             Uri endpoint = new Uri(request);
@@ -34,6 +38,123 @@ namespace ServiceLayer.Database
                 else { return new List<PStudentAcademicInfoModel<PNameModel>>(); }
             }
         }
+
+        public async Task<PStudentAcademicInfoModel<PNameModel>> GetByParams(PRStudentAcademicInfoParams paramsModel)
+        {
+            string request = $"{_webAddress}/GetByParams";
+            HandleRequestParameter(ref request, paramsModel);
+
+            Uri endpoint = new Uri(request);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<PStudentAcademicInfoModel<PNameModel>>(jsonResponse);
+                }
+                return new PStudentAcademicInfoModel<PNameModel>();
+            }
+        }
+
+        public async Task<int> GetRecordId(PRStudentAcademicInfoParams paramsModel)
+        {
+            string request = $"{_webAddress}/GetRecordId";
+            HandleRequestParameter(ref request, paramsModel);
+
+            Uri endpoint = new Uri(request);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<int>(jsonResponse);
+                }
+                return -1;
+            }
+        }
+
+        public async Task<bool> InsertNew(PStudentAcademicInfoModel<PNameModel> paramsModel)
+        {
+            string request = $"{_webAddress}/InsertNew";
+
+            string JsonParameter = JsonConvert.SerializeObject(paramsModel, Formatting.Indented);
+            StringContent content = new StringContent(JsonParameter, Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.PostAsync(request, content);
+
+                return response.IsSuccessStatusCode;
+            }
+        }
+
+        public async Task<bool> Update(PStudentAcademicInfoModel<PNameModel> paramsModel)
+        {
+            Uri request = new Uri($"{_webAddress}/Update");
+
+            string JsonParameter = JsonConvert.SerializeObject(paramsModel, Formatting.Indented);
+            StringContent content = new StringContent(JsonParameter, Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage httpRequest = new HttpRequestMessage
+                {
+                    Method = new HttpMethod("Patch"),
+                    RequestUri = request,
+                    Content = content
+                };
+
+                HttpResponseMessage response = await client.PostAsync(request, content);
+                response.EnsureSuccessStatusCode();
+
+                return response.IsSuccessStatusCode;
+            }
+        }
+
+        public async Task<bool> Delete(PRStudentAcademicInfoParams paramsModel)
+        {
+            Uri requestUri = new Uri($"{_webAddress}/Delete");
+
+            string JsonParameter = JsonConvert.SerializeObject(paramsModel, Formatting.Indented);
+            StringContent content = new StringContent(JsonParameter, Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Delete,
+                    RequestUri = requestUri,
+                    Content = content
+                };
+                HttpResponseMessage response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                return response.IsSuccessStatusCode;
+            }
+        }
+
+        #region Helpers
+        private void HandleRequestParameter(ref string request, PRStudentAcademicInfoParams paramsModel)
+        {
+            if (String.IsNullOrEmpty(paramsModel.SrCode))
+                request += $"/SrCode={paramsModel.SrCode}";
+            
+            if (String.IsNullOrEmpty(paramsModel.AcademicYear))
+                request += $"&AcademicYear{paramsModel.AcademicYear.Replace(' ', '%')}";
+
+            if (String.IsNullOrEmpty(paramsModel.YearLevel))
+                request += $"&YearLevel={paramsModel.YearLevel}";
+
+            if (String.IsNullOrEmpty(paramsModel.Semester))
+                request += $"&Semester={paramsModel.Semester}";
+        }
+        #endregion
 
 
         private string _webAddress;
