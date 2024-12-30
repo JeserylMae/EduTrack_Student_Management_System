c93079a4d13ea200a4a001b96b5c11dd67f119da
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-30 17:34:19 +0800

    created an api method for program table. FINISHED

---
+++ src/InfrastructureLayer/Database/IProgramRepository.cs
@@ -0,0 +1,14 @@
+﻿using DomainLayer.DataModels;
+
+namespace InfrastructureLayer.Database
+{
+    public interface IProgramRepository
+    {
+        Task<int> Delete(string ProgramId);
+        Task<List<PRProgramModel>> GetAll();
+        Task<Dictionary<dynamic, dynamic>> GetAllProgram();
+        Task<int> InsertNew(PRProgramModel programModel);
+        Task<int> Update(PRProgramModel programModel);
+        Task<int> UpdateProgramId(PRProgramModel programModel);
+    }
+}
