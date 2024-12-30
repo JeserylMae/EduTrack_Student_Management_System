eb6439d24b693824040de0c5a2ea9c36495105e6
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-30 17:33:55 +0800

    Added program repository to builder transient. FINISHED

--- src/InfrastructureLayer/Data/BuilderServices.cs
+++ src/InfrastructureLayer/Data/BuilderServices.cs
@@ -9,6 +9,7 @@ namespace InfrastructureLayer.Data
         internal static void AddTransientServices(this WebApplicationBuilder builder)
         {
             builder.Services.AddTransient<IUserRepository, MUserRepository>();
+            builder.Services.AddTransient<IProgramRepository, MProgramRepository>();
             builder.Services.AddTransient<IEndpointRepository, MEndpointRepository>();
             builder.Services.AddTransient<IStudentAcademicInfoRepository, MStudentAcademicInfoRepository>();
             builder.Services.AddTransient<IStudentPersonalInfoRepository, MStudentPersonalInfoRepository>();
