0caa79af446a6f33652cfbd813ce8ac44c8e481c
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-30 17:42:54 +0800

    Added new service. FINISHED

---
+++ src/ServiceLayer/Database/ProrgamServices.cs
@@ -0,0 +1,42 @@
+﻿using DomainLayer.DataModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Database
+{
+    public class ProrgamServices
+    {
+        public ProrgamServices()
+        {
+            _webAddress = $"{DatabaseConnection.GetWebAddress()}/Program";
+        }
+
+
+        public async Task<List<PRProgramModel>> GetAllProgram()
+        {
+            string request = $"{_webAddress}/GetAllProgram";
+
+            using (HttpClient client = new HttpClient()) 
+            { 
+                HttpResponseMessage response = await client.GetAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<PRProgramModel>>(jsonResponse);
+                }
+                return new List<PRProgramModel>();
+            }
+        }
+
+
+        private string _webAddress;
+    }
+}
