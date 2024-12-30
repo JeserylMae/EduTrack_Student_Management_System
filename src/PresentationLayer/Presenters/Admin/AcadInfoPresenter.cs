9d22c4bc8e5af3170116be6c770527aae2af146f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-30 18:21:23 +0800

    Added options to combo boxes. FINISHED

--- src/PresentationLayer/Presenters/Admin/AcadInfoPresenter.cs
+++ src/PresentationLayer/Presenters/Admin/AcadInfoPresenter.cs
@@ -1,6 +1,7 @@
 ﻿using PresentationLayer.Presenters.Enumerations;
 using PresentationLayer.Presenters.General;
 using PresentationLayer.UserControls.AdminSubControls;
+using ServiceLayer.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,10 +30,31 @@ namespace PresentationLayer.Presenters.Admin
             if (_studentAcadInfoControl.CurrentRequestType == FormRequestType.UPDATE)
                 SetNameTextBoxToReadOnly();
 
+            LoadComboBoxOptions();
         }
 
 
         #region Helpers
+        private async void LoadComboBoxOptions()
+        {
+            string[] yearOptions = { "FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH"};
+            _studentAcadInfoControl.AccessYearComboBox.Items.AddRange(yearOptions);
+
+            string[] semesterOptions = { "FISRT", "SECOND", "SUMMER" };
+            _studentAcadInfoControl.AccessSemesterComboBox.Items.AddRange(semesterOptions);
+
+            int startingYear = DateTime.Now.Year + 1;
+            string[] acadYearOptions = Enumerable.Range(0, 9)
+                .Select(i => $"A.Y. {startingYear - (i + 1)} - {startingYear - i}")
+                .ToArray();
+            _studentAcadInfoControl.AccessAcademicYearComboBox.Items.AddRange(acadYearOptions);
+
+            ProrgamServices services = new ProrgamServices();
+            Dictionary<string, string> programList = await services.GetAllProgram();
+            string[] programOptions = programList.Values.ToArray();
+            _studentAcadInfoControl.AccessProgramComboBox.Items.AddRange(programOptions);
+        }
+
         private void SetNameTextBoxToReadOnly()
         {
             _studentAcadInfoControl.AccessLastNameTextBox.ReadOnly = true;
