f4003ab766d31ef99c72e1ec15ad4e4884f80371
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-30 17:34:46 +0800

    Created a query list for interacting with program table. FINISHED

---
+++ src/InfrastructureLayer/Query/ProgramQuery.cs
@@ -0,0 +1,58 @@
+﻿namespace InfrastructureLayer.Query
+{
+    public struct ProgramQuery
+    {
+        public readonly string GetAll => (@"
+            SELECT * FROM ProgramTbl;
+        ");
+
+        public readonly string GetAllProgram => (@"
+            SELECT 
+                ProgramId, ProgramName
+            From 
+                ProgramTbl;
+        ");
+
+        public readonly string InsertNew => (@"
+            INSERT INTO ProgramTbl(
+                ProgramId, 
+                ProgramName, 
+                DepartmentId, 
+                DepartmentName
+            )
+            VALUES (
+                @p_ProgramId, 
+                @p_ProgramName, 
+                @p_DepartmentId, 
+                @p_DepartmentName
+            );
+        ");
+
+        public readonly string Update => (@"
+            UPDATE 
+                ProgramTbl
+            SET 
+                ProgramName = @p_ProgramName,
+                DepartmentId = @p_DepartmentId,
+                DepartmentName = @p_DepartmentName
+            WHERE 
+                ProgramId = @p_ProgramId;
+        ");
+
+        public readonly string UpdateProgramId => (@"
+            UPDATE 
+                ProgramTbl
+            SET 
+                ProgramId = @p_ProgramId
+            WHERE
+                ProgramName = @p_ProgramName;
+        ");
+
+        public readonly string Delete => (@"
+            DELETE FROM 
+                ProgramTbl
+            WHERE 
+                ProgramId = @p_ProgramId;
+        ");
+    }
+}
