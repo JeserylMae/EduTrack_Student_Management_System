7c0d52e37af457efda7e19de20c4c4bb20293279
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-02 12:24:35 +0800

    Created a new user control for filter. FINISHED

---
+++ src/PresentationLayer/UserControls/General/FilterControl.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PresentationLayer.UserControls.General
+{
+    partial class FilterControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.AcademicYearComboBox = new System.Windows.Forms.ComboBox();
+            this.YearLevelComboBox = new System.Windows.Forms.ComboBox();
+            this.SemesterComboBox = new System.Windows.Forms.ComboBox();
+            this.SectionComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.iconPictureBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(145)))), ((int)(((byte)(164)))));
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Filter;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(145)))), ((int)(((byte)(164)))));
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 24;
+            this.iconPictureBox1.Location = new System.Drawing.Point(8, 0);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(24, 24);
+            this.iconPictureBox1.TabIndex = 0;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // AcademicYearComboBox
+            // 
+            this.AcademicYearComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.AcademicYearComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.AcademicYearComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.AcademicYearComboBox.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AcademicYearComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(145)))), ((int)(((byte)(164)))));
+            this.AcademicYearComboBox.FormattingEnabled = true;
+            this.AcademicYearComboBox.Location = new System.Drawing.Point(53, 1);
+            this.AcademicYearComboBox.Name = "AcademicYearComboBox";
+            this.AcademicYearComboBox.Size = new System.Drawing.Size(158, 26);
+            this.AcademicYearComboBox.TabIndex = 1;
+            this.AcademicYearComboBox.Text = "Academic Year";
+            // 
+            // YearLevelComboBox
+            // 
+            this.YearLevelComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.YearLevelComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.YearLevelComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.YearLevelComboBox.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YearLevelComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(145)))), ((int)(((byte)(164)))));
+            this.YearLevelComboBox.FormattingEnabled = true;
+            this.YearLevelComboBox.Location = new System.Drawing.Point(231, 1);
+            this.YearLevelComboBox.Name = "YearLevelComboBox";
+            this.YearLevelComboBox.Size = new System.Drawing.Size(158, 26);
+            this.YearLevelComboBox.TabIndex = 2;
+            this.YearLevelComboBox.Text = "Year Level";
+            // 
+            // SemesterComboBox
+            // 
+            this.SemesterComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.SemesterComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.SemesterComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.SemesterComboBox.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SemesterComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(145)))), ((int)(((byte)(164)))));
+            this.SemesterComboBox.FormattingEnabled = true;
+            this.SemesterComboBox.Location = new System.Drawing.Point(409, 1);
+            this.SemesterComboBox.Name = "SemesterComboBox";
+            this.SemesterComboBox.Size = new System.Drawing.Size(158, 26);
+            this.SemesterComboBox.TabIndex = 3;
+            this.SemesterComboBox.Text = "Semester";
+            // 
+            // SectionComboBox
+            // 
+            this.SectionComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.SectionComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.SectionComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.SectionComboBox.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SectionComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(145)))), ((int)(((byte)(164)))));
+            this.SectionComboBox.FormattingEnabled = true;
+            this.SectionComboBox.Location = new System.Drawing.Point(587, 1);
+            this.SectionComboBox.Name = "SectionComboBox";
+            this.SectionComboBox.Size = new System.Drawing.Size(158, 26);
+            this.SectionComboBox.TabIndex = 4;
+            this.SectionComboBox.Text = "Section";
+            // 
+            // FilterControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(253)))));
+            this.Controls.Add(this.SectionComboBox);
+            this.Controls.Add(this.SemesterComboBox);
+            this.Controls.Add(this.YearLevelComboBox);
+            this.Controls.Add(this.AcademicYearComboBox);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "FilterControl";
+            this.Padding = new System.Windows.Forms.Padding(1);
+            this.Size = new System.Drawing.Size(1335, 29);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.ComboBox AcademicYearComboBox;
+        private System.Windows.Forms.ComboBox YearLevelComboBox;
+        private System.Windows.Forms.ComboBox SemesterComboBox;
+        private System.Windows.Forms.ComboBox SectionComboBox;
+    }
+}
