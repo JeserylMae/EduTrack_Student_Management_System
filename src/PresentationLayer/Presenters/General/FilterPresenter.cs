547404384bb6de8d6b957f583c1d0b94432bdd24
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-02 12:23:28 +0800

    Created an empty class for filter presenter. FINISHED

---
+++ src/PresentationLayer/Presenters/General/FilterPresenter.cs
@@ -0,0 +1,19 @@
+﻿using PresentationLayer.UserControls.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Presenters.General
+{
+    internal class FilterPresenter
+    {
+        public FilterPresenter(IFilterControl filterControl)
+        {
+            _filterControl = filterControl;
+        }
+
+        private IFilterControl _filterControl;
+    }
+}
