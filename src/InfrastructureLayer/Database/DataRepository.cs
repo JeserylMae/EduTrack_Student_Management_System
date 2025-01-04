8a7a38c15d142e3b515ecab314bb425aaa6db026
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-04 13:09:01 +0800

    Added new class for repository interaction. FINISHED

---
+++ src/InfrastructureLayer/Database/DataRepository.cs
@@ -0,0 +1,80 @@
+﻿
+using Dapper;
+using System.Data;
+using InfrastructureLayer.Data;
+
+namespace InfrastructureLayer.Database
+{
+    public class DataRepository : IDataRepository
+    {
+        public DataRepository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+
+        public async Task<int> CheckDatabaseConnection()
+        {
+            string query = "SELECT 1";
+
+            using IDbConnection connection = _databaseContext.CreateConnection();
+            int result = await connection.ExecuteScalarAsync<int>(query);
+
+            return result;
+        }
+
+        public async Task<List<TModel>> GetAll<TModel>(string procedure) where TModel : class
+        {
+            using (IDbConnection connection = _databaseContext.CreateConnection())
+            {
+                var result = await connection.QueryAsync<TModel>(
+                    procedure, commandType: CommandType.Text
+                );
+                return result.ToList();
+            }
+        }
+
+        public async Task<Dictionary<dynamic, dynamic>> GetAll(string procedure)
+        {
+            using (IDbConnection connection = _databaseContext.CreateConnection())
+            {
+                var result = await connection.QueryAsync(
+                    procedure, commandType: CommandType.Text
+                );
+                return result.ToDictionary(
+                    row => (dynamic)row.ProgramId,
+                    row => (dynamic)row.ProgramName
+                );
+            }
+        }
+
+        public async Task<TModel> GetSingle<TModel>(string procedure, DynamicParameters parameters)
+        {
+
+            using (IDbConnection connection = _databaseContext.CreateConnection())
+            {
+                var response = await connection.QuerySingleOrDefaultAsync<TModel>(
+                    procedure, param: parameters,
+                    commandType: CommandType.Text
+                );
+
+                if (response == null)
+                    throw new NullReferenceException("Failed to get any data.");
+                else return response;
+            }
+        }
+
+        public async Task<int> Execute(string procedure, DynamicParameters parameters)
+        {
+            using (IDbConnection connection = _databaseContext.CreateConnection())
+            {
+                return await connection.ExecuteAsync(
+                    procedure, parameters,
+                    commandType: CommandType.Text);
+            }
+        }
+
+
+        private DatabaseContext _databaseContext;
+    }
+}
---
+++ src/InfrastructureLayer/Database/IDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dapper;
+
+namespace InfrastructureLayer.Database
+{
+    public interface IDataRepository
+    {
+        Task<int> CheckDatabaseConnection();
+        Task<int> Execute(string procedure, DynamicParameters parameters);
+        Task<Dictionary<dynamic, dynamic>> GetAll(string procedure);
+        Task<List<TModel>> GetAll<TModel>(string procedure) where TModel : class;
+        Task<TModel> GetSingle<TModel>(string procedure, DynamicParameters parameters);
+    }
+}
