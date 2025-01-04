63b6179d2089b87f059b40b01c2936f494c40da9
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-04 18:15:09 +0800

    Changed the directory of classess. FINISHED

---
+++ src/DomainLayer/DataModels/General/PRUserModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DomainLayer.DataModels
+{
+    public class PRUserModel
+    {
+        public string UserID { get; set; }
+        public string EmailAddress { get; set; }
+        public string AccountPassword { get; set; }
+        public string Position { get; set; }
+    }
+}
