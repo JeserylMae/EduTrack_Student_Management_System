3c9ff659dfffe6067758207664ca66eb32ba7159
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-04 18:16:12 +0800

    Added new models for instructor personal info. FINISHED

---
+++ src/DomainLayer/DataModels/Instructor/PInstructorPersonalInfoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels.Instructor
+{
+    public class PInstructorPersonalInfoModel<TModel>
+    {
+        public TModel InfoModel         { get; set; }
+        public string DefaultPassword   { get; set; }
+        public string Position          { get; set; }
+        public string UserId            { get; set; }
+        public string GuardianCode      { get; set; }
+    }
+}
---
+++ src/DomainLayer/DataModels/Instructor/PInstructorPersonalInfoParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels.Instructor
+{
+    public class PInstructorPersonalInfoParams
+    {
+        public string ItrCode               { get; set; }
+        public string UserNameCode          { get; set; }
+        public string UserAddressCode       { get; set; }
+        public string GuardianNameCode      { get; set; }
+        public string GuardianAddressCode   { get; set; }
+    }
+}
