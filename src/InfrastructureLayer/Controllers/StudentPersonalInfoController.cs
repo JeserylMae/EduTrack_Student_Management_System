d2ca2d158e88e6c966000abf7baf1b052f14d37c
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-04 18:16:43 +0800

    Fixed white spaces. FINISHED

--- src/InfrastructureLayer/Controllers/StudentPersonalInfoController.cs
+++ src/InfrastructureLayer/Controllers/StudentPersonalInfoController.cs
@@ -91,50 +91,50 @@ namespace InfrastructureLayer.Controllers
         private void AddValuesToParameters(ref DynamicParameters parameters,
                                     ref PStudentPersonalInfoParams codes)
         {
-            parameters.Add("@p_SrCode", codes.SrCode);
-            parameters.Add("@p_StudentNameCode", codes.StudentNameCode);
-            parameters.Add("@p_StudentAddressCode", codes.StudentAddressCode);
-            parameters.Add("@p_GuardianNameCode", codes.GuardianNameCode);
-            parameters.Add("@p_GuardianAddressCode", codes.GuardianAddressCode);
+            parameters.Add("@p_SrCode",                 codes.SrCode                );
+            parameters.Add("@p_StudentNameCode",        codes.UserNameCode          );
+            parameters.Add("@p_StudentAddressCode",     codes.UserAddressCode       );
+            parameters.Add("@p_GuardianNameCode",       codes.GuardianNameCode      );
+            parameters.Add("@p_GuardianAddressCode",    codes.GuardianAddressCode   );
         }
 
         private void AddValuesToParameters(ref DynamicParameters parameters,
                     ref PStudentPersonalInfoModel<RStudentPersonalInfoModel> student,
                     RequestType request)
         {
-            parameters.Add("@p_SrCode", student.InfoModel.SrCode);
-            parameters.Add("@p_LastName", student.InfoModel.LastName);
-            parameters.Add("@p_FirstName", student.InfoModel.FirstName);
-            parameters.Add("@p_MiddleName", student.InfoModel.MiddleName);
-            parameters.Add("@p_BirthDate", student.InfoModel.BirthDate);
-            parameters.Add("@p_Gender", student.InfoModel.Gender);
-            parameters.Add("@p_ContactNumber", student.InfoModel.ContactNumber);
-            parameters.Add("@p_EmailAddress", student.InfoModel.EmailAddress);
-            parameters.Add("@p_HouseNumber", student.InfoModel.HouseNumber);
-            parameters.Add("@p_Barangay", student.InfoModel.Barangay);
-            parameters.Add("@p_Municipality", student.InfoModel.Municipality);
-            parameters.Add("@p_Province", student.InfoModel.Province);
-            parameters.Add("@p_GuardianLastName", student.InfoModel.GuardianLastName);
-            parameters.Add("@p_GuardianFirstName", student.InfoModel.GuardianFirstName);
-            parameters.Add("@p_GuardianMiddleName", student.InfoModel.GuardianMiddleName);
-            parameters.Add("@p_GuardianContactNumber", student.InfoModel.GuardianContactNumber);
-            parameters.Add("@p_GuardianHouseNumber", student.InfoModel.GuardianHouseNumber);
-            parameters.Add("@p_GuardianBarangay", student.InfoModel.GuardianBarangay);
-            parameters.Add("@p_GuardianMunicipality", student.InfoModel.GuardianMunicipality);
-            parameters.Add("@p_GuardianProvince", student.InfoModel.GuardianProvince);
-            parameters.Add("@p_StudentNameCode", student.UserId);
-            parameters.Add("@p_StudentAddressCode", student.UserId);
-            parameters.Add("@p_GuardianNameCode", student.GuardianCode);
-            parameters.Add("@p_GuardianAddressCode", student.GuardianCode);
+            parameters.Add("@p_SrCode",                 student.InfoModel.SrCode                );
+            parameters.Add("@p_LastName",               student.InfoModel.LastName              );
+            parameters.Add("@p_FirstName",              student.InfoModel.FirstName             );
+            parameters.Add("@p_MiddleName",             student.InfoModel.MiddleName            );
+            parameters.Add("@p_BirthDate",              student.InfoModel.BirthDate             );
+            parameters.Add("@p_Gender",                 student.InfoModel.Gender                );
+            parameters.Add("@p_ContactNumber",          student.InfoModel.ContactNumber         );
+            parameters.Add("@p_EmailAddress",           student.InfoModel.EmailAddress          );
+            parameters.Add("@p_HouseNumber",            student.InfoModel.HouseNumber           );
+            parameters.Add("@p_Barangay",               student.InfoModel.Barangay              );
+            parameters.Add("@p_Municipality",           student.InfoModel.Municipality          );
+            parameters.Add("@p_Province",               student.InfoModel.Province              );
+            parameters.Add("@p_GuardianLastName",       student.InfoModel.GuardianLastName      );
+            parameters.Add("@p_GuardianFirstName",      student.InfoModel.GuardianFirstName     );
+            parameters.Add("@p_GuardianMiddleName",     student.InfoModel.GuardianMiddleName    );
+            parameters.Add("@p_GuardianContactNumber",  student.InfoModel.GuardianContactNumber );
+            parameters.Add("@p_GuardianHouseNumber",    student.InfoModel.GuardianHouseNumber   );
+            parameters.Add("@p_GuardianBarangay",       student.InfoModel.GuardianBarangay      );
+            parameters.Add("@p_GuardianMunicipality",   student.InfoModel.GuardianMunicipality  );
+            parameters.Add("@p_GuardianProvince",       student.InfoModel.GuardianProvince      );
+            parameters.Add("@p_StudentNameCode",        student.UserId                          );
+            parameters.Add("@p_StudentAddressCode",     student.UserId                          );
+            parameters.Add("@p_GuardianNameCode",       student.GuardianCode                    );
+            parameters.Add("@p_GuardianAddressCode",    student.GuardianCode                    );
 
             if (RequestType.INSERT == request)
             {
                 int currentYear = DateTime.Now.Year;
                 string AcademicYear = $"A.Y. {currentYear - 1}-{currentYear}";
 
-                parameters.Add("@p_AcademicYear", AcademicYear);
-                parameters.Add("@p_DefaultPassword", student.DefaultPassword);
-                parameters.Add("@p_Position", student.Position);
+                parameters.Add("@p_AcademicYear",       AcademicYear            );
+                parameters.Add("@p_DefaultPassword",    student.DefaultPassword );
+                parameters.Add("@p_Position",           student.Position        );
             }
         }
         #endregion
