d4988bce4b8eca343c101b981f7f7ef161d9341d
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-08 13:22:56 +0800

    Added new model. FINISHED

---
+++ src/DomainLayer/DataModels/Instructor/InstructorAcademicParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels.Instructor
+{
+    public class InstructorAcademicParams
+    {
+        public string ItrCode       { get; set; } 
+        public string Course        { get; set; }
+        public string Section       { get; set; }
+        public string YearLevel     { get; set; }
+        public string AcademicYear  { get; set; }
+    }
+}
