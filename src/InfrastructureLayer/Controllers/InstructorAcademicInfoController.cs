7e53f6491368fec3801d9849ebe06dd18eae7b8b
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-08 13:24:01 +0800

    Initialized new controller for instructor academic info. FINISHED

---
+++ src/InfrastructureLayer/Controllers/InstructorAcademicInfoController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfrastructureLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructorAcademicInfoController : ControllerBase
+    {
+        public InstructorAcademicInfoController()
+        {
+            
+        }
+    }
+}
