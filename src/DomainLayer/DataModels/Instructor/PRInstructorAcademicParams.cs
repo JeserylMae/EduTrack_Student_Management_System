ec1d4917328f317d98b08cd766ffd6f527374ed7
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-09 11:57:56 +0800

    update

---
+++ src/DomainLayer/DataModels/Instructor/PRInstructorAcademicParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels.Instructor
+{
+    public class PRInstructorAcademicParams
+    {
+        public string ItrCode       { get; set; } 
+        public string Course        { get; set; }
+        public string Section       { get; set; }
+        public string YearLevel     { get; set; }
+        public string AcademicYear  { get; set; }
+    }
+}
