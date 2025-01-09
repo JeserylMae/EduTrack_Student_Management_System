386c55ebff536997810e05fc5e7799627b71317f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-09 11:57:11 +0800

    Added new model for instructor academic info. FINISHED

---
+++ src/DomainLayer/DataModels/Instructor/PInstructorAcademicInfoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DataModels.Instructor
+{
+    public class PInstructorAcademicInfoModel<TModel>
+    {
+        public string ItrCode { get; set; }
+        public string Course {  get; set; }
+        public string Program { get; set; }
+        public string Section { get; set; }
+        public string YearLevel { get; set; }
+        public string AcademicYear { get; set; }
+        public TModel InstructorName { get; set; }
+    }
+}
