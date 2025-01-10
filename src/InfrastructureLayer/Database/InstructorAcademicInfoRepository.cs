3dd8c9d03cfe67b278102a731949eb422da8fb7e
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-10 12:59:17 +0800

    Added new database method. FINSIHED

---
+++ src/InfrastructureLayer/Database/InstructorAcademicInfoRepository.cs
@@ -0,0 +1,45 @@
+﻿
+using Dapper;
+using DomainLayer.DataModels;
+using DomainLayer.DataModels.Instructor;
+using InfrastructureLayer.Data;
+using InfrastructureLayer.Query;
+using System.Data;
+
+namespace InfrastructureLayer.Database
+{
+    public class InstructorAcademicInfoRepository : IInstructorAcademicInfoRepository
+    {
+        public InstructorAcademicInfoRepository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+            _query = new InstructorAcademicInfoQuery();
+        }
+
+
+        public async Task<List<PInstructorAcademicInfoModel<PNameModel>>> GetAll()
+        {
+            string procedure = _query.spGetAll;
+
+            using (IDbConnection connection = _databaseContext.CreateConnection())
+            {
+                var result = await connection.QueryAsync<PInstructorAcademicInfoModel<PNameModel>,
+                                             PNameModel, PInstructorAcademicInfoModel<PNameModel>>(
+                    procedure,
+                    (academicInfo, name) =>
+                    {
+                        academicInfo.InstructorName = name;
+                        return academicInfo;
+                    }, splitOn: "LastName",
+                    commandType: CommandType.Text
+                );
+                return result.ToList();
+            }
+        }
+
+       
+
+        private DatabaseContext _databaseContext;
+        private InstructorAcademicInfoQuery _query;
+    }
+}
