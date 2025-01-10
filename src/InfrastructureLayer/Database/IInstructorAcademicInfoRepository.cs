0eb97ac802d1f4a129df4a1c1bdfc9036e0bc231
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-10 12:58:57 +0800

    Added new database method. FINISHED

---
+++ src/InfrastructureLayer/Database/IInstructorAcademicInfoRepository.cs
@@ -0,0 +1,10 @@
+﻿using DomainLayer.DataModels;
+using DomainLayer.DataModels.Instructor;
+
+namespace InfrastructureLayer.Database
+{
+    public interface IInstructorAcademicInfoRepository
+    {
+        Task<List<PInstructorAcademicInfoModel<PNameModel>>> GetAll();
+    }
+}
