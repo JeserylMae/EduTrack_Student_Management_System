92f9300e596ba105962e0c78873f8b01440bc55c
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-11 11:15:31 +0800

    Fixed return type and whaite spaces. FINISHED

--- src/InfrastructureLayer/Database/IDataRepository.cs
+++ src/InfrastructureLayer/Database/IDataRepository.cs
@@ -5,9 +5,13 @@ namespace InfrastructureLayer.Database
     public interface IDataRepository
     {
         Task<int> CheckDatabaseConnection();
+
         Task<Dictionary<dynamic, dynamic>> GetAll(string procedure);
+        
         Task<int> Execute(string procedure, DynamicParameters parameters);
-        Task<List<TModel>> GetAll<TModel>(string procedure) where TModel : class;
+        
         Task<TModel> GetSingle<TModel>(string procedure, DynamicParameters parameters);
+        
+        Task<List<TModel>> GetAll<TModel>(string procedure, DynamicParameters? parameters = null) where TModel : class;
     }
 }
