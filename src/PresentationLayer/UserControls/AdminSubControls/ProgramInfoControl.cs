1658587a57af62a986adca14852a21feca8cfc03
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-13 16:35:53 +0800

    Implemented interface. FINISHED

--- src/PresentationLayer/UserControls/AdminSubControls/ProgramInfoControl.cs
+++ src/PresentationLayer/UserControls/AdminSubControls/ProgramInfoControl.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace PresentationLayer.UserControls.AdminSubControls
 {
-    public partial class ProgramInfoControl : UserControl
+    public partial class ProgramInfoControl : UserControl, IProgramInfoControl
     {
         public ProgramInfoControl()
         {
             InitializeComponent();
         }
+
+        public object[] AccessInfoTableRowData 
+        { 
+            set
+            {
+                InfoTable.Rows.Add(value);
+                InfoTable.Rows[InfoTable.RowCount - 1].Height = 28;
+            } 
+        }
+
+        public DataGridView AccessInfoTable
+        {
+            get => InfoTable;
+        }
+
+        public TextBox AccessSearchProgramIdTextbox 
+        {
+            get => SearchProgramIdTextbox;
+        }
+
+
+        public event EventHandler ExitButtonClicked;
+        public event EventHandler CloseEditorButtonClicked;
+        public event EventHandler ProgramInfoButtonClicked;
+        public event EventHandler OpenAddFormButtonClicked;
+        public event EventHandler OpenDropFormButtonClicked;
+        public event EventHandler FileDropDownButtonClicked;
+        public event EventHandler OpenModifyFormButtonClicked;
+        public event EventHandler SearchProgramIdButtonClicked;
+        public event EventHandler InstructorAcadInfoButtonClicked;
+        public event EventHandler StudentAcademicInfoButtonClicked;
+        public event EventHandler StudentPersonalInfoButtonClicked;
+        public event EventHandler InstructorPersonalInfoButtonClicked;
+        public event KeyboardEventHandler SearchProgramIdTextboxPressed;
     }
 }
