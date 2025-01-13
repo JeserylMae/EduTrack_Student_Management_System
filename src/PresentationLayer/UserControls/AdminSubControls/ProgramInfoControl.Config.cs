7ed83ae2e45b699f2dd3fdc8a0419a00eda9db72
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-13 18:49:53 +0800

    Assigned event handlers to buttons. FINISHED

--- src/PresentationLayer/UserControls/AdminSubControls/ProgramInfoControl.Config.cs
+++ src/PresentationLayer/UserControls/AdminSubControls/ProgramInfoControl.Config.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,6 +11,46 @@ namespace PresentationLayer.UserControls.AdminSubControls
 {
     partial class ProgramInfoControl
     {
+        private void InitializedElementCreated()
+        {
+            if (FileDropDownLayout != null) FileDropDownLayoutCreated.TrySetResult(true);
+            if (OpenAddFormButton != null) OpenAddFormButtonCreated.TrySetResult(true);
+            if (OpenDropFormButton != null) OpenDropFormButtonCreated.TrySetResult(true);
+            if (OpenModifyFormButton != null) OpenModifyFormButtonCreated.TrySetResult(true);
+            if (SearchProgramIdButton != null) SearchProgramIdButtonCreated.TrySetResult(true);
+            if (SearchProgramIdTextbox != null) SearchProgramIdTextBoxCraeted.TrySetResult(true);
+        }
+
+        private async Task InitializedButtonSubscriber()
+        {
+            await FileDropDownLayoutCreated.Task;
+            ExitButton.Click += delegate { ExitButtonClicked?.Invoke(this, EventArgs.Empty); };
+            CloseEditorButton.Click += delegate { CloseEditorButtonClicked?.Invoke(this, EventArgs.Empty); };
+            ProgramInfoButton.Click += delegate { ProgramInfoButtonClicked?.Invoke(this, EventArgs.Empty); };
+            FileDropDownButton.Click += delegate { FileDropDownButtonClicked?.Invoke(this, EventArgs.Empty); };
+            InstructorAcadInfoButton.Click += delegate { InstructorAcadInfoButtonClicked?.Invoke(this, EventArgs.Empty); };
+            StudentAcademicInfoButton.Click += delegate { StudentAcademicInfoButtonClicked?.Invoke(this, EventArgs.Empty); };
+            StudentPersonalInfoButton.Click += delegate { StudentPersonalInfoButtonClicked?.Invoke(this, EventArgs.Empty); };
+            InstructorPersonalInfoButton.Click += delegate { InstructorPersonalInfoButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await OpenAddFormButtonCreated.Task;
+            OpenAddFormButton.Click += delegate { OpenAddFormButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await OpenDropFormButtonCreated.Task;
+            OpenDropFormButton.Click += delegate { OpenDropFormButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await OpenModifyFormButtonCreated.Task;
+            OpenModifyFormButton.Click += delegate { OpenModifyFormButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await SearchProgramIdButtonCreated.Task;
+            SearchProgramIdButton.Click += delegate { SearchProgramIdButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await SearchProgramIdTextBoxCraeted.Task;
+            SearchProgramIdTextbox.KeyDown += delegate (object sender, KeyEventArgs e) { SearchProgramIdTextboxPressed?.Invoke(sender, e); };
+
+            this.Load += delegate { OnControlLoad?.Invoke(this, EventArgs.Empty); };
+        }
+
         private void InitializeInfoTable()
         {
             InfoTable.ScrollBars = ScrollBars.Both;
@@ -51,9 +92,9 @@ namespace PresentationLayer.UserControls.AdminSubControls
         private void ModifyDataGridViewColumnWidth()
         {
             InfoTable.Columns["ProgramId"].Width        = 200;
-            InfoTable.Columns["ProgramName"].Width      = 400;
+            InfoTable.Columns["ProgramName"].Width      = 800;
             InfoTable.Columns["DepartmentId"].Width     = 200;
-            InfoTable.Columns["DepartmentName"].Width   = 400;
+            InfoTable.Columns["DepartmentName"].Width   = 800;
         }
     }
 }
