909d3a0bf917e44d14560c0b8dd8d0be71089e3c
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-13 16:34:47 +0800

    Added interface for program info control. FINISHED

---
+++ src/PresentationLayer/UserControls/AdminSubControls/IProgramInfoControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace PresentationLayer.UserControls.AdminSubControls
+{
+    internal interface IProgramInfoControl
+    {
+        event EventHandler ExitButtonClicked;
+        event EventHandler CloseEditorButtonClicked;
+        event EventHandler ProgramInfoButtonClicked;
+        event EventHandler OpenAddFormButtonClicked;
+        event EventHandler OpenDropFormButtonClicked;
+        event EventHandler FileDropDownButtonClicked;
+        event EventHandler OpenModifyFormButtonClicked;
+        event EventHandler SearchProgramIdButtonClicked;
+        event EventHandler InstructorAcadInfoButtonClicked;
+        event EventHandler StudentAcademicInfoButtonClicked;
+        event EventHandler StudentPersonalInfoButtonClicked;
+        event EventHandler InstructorPersonalInfoButtonClicked;
+        event KeyboardEventHandler SearchProgramIdTextboxPressed;
+
+        object[] AccessInfoTableRowData      { set; }
+        DataGridView AccessInfoTable         { get; }
+        TextBox AccessSearchProgramIdTextbox { get; }
+    }
+}
