7cb8e28f3006340e3d0cb76a0dc40c8b5b5df194
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-13 17:50:40 +0800

    Added methods for api request. FINISHED

--- src/ServiceLayer/Database/ProrgamServices.cs
+++ src/ServiceLayer/Database/ProrgamServices.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
@@ -18,6 +19,23 @@ namespace ServiceLayer.Database
             _webAddress = $"{DatabaseConnection.GetWebAddress()}/Program";
         }
 
+
+        public async Task<List<PRProgramModel>> GetAll()
+        {
+            Uri endpoint = new Uri($"{_webAddress}/GetAll");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<PRProgramModel>>(jsonResponse);
+                }
+                return new List<PRProgramModel>();
+            }
+        }
 
         public async Task<Dictionary<string, string>> GetAllProgram()
         {
@@ -33,6 +51,58 @@ namespace ServiceLayer.Database
                     return JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonResponse);
                 }
                 return new Dictionary<string, string>();
+            }
+        }
+
+        public async Task<bool> InsertNew(PRProgramModel program)
+        {
+            string request = $"{_webAddress}/InsertNew";
+
+            string jsonContent = JsonConvert.SerializeObject(program, Formatting.Indented);
+            StringContent content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.PostAsync(request, content);
+                return response.IsSuccessStatusCode;
+            }
+        }
+
+        public async Task<bool> Update(PRProgramModel program)
+        {
+            Uri endpoint = new Uri($"{_webAddress}/Update");
+
+            string jsonContent = JsonConvert.SerializeObject(program, Formatting.Indented);
+            StringContent content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage httpRequest = new HttpRequestMessage
+                {
+                    Method = new HttpMethod("Patch"),
+                    Content = content,
+                    RequestUri = endpoint
+                };
+
+                HttpResponseMessage response = await client.SendAsync(httpRequest);
+                return response.IsSuccessStatusCode;
+            }
+        }
+
+        public async Task<bool> Delete(string programId)
+        {
+            Uri endpoint = new Uri($"{_webAddress}/Delete?programId={programId}");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage httpRequest = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Delete,
+                    RequestUri = endpoint
+                };
+
+                HttpResponseMessage response = await client.SendAsync(httpRequest);
+                return response.IsSuccessStatusCode;
             }
         }
 
