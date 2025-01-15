ba72894c2f4ec78f5e35d7d85520ebe012b4e939
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-15 17:33:12 +0800

    Added new event handler. FINISHED

--- src/PresentationLayer/UserControls/AdminSubControls/IProgramInfoFormControl.cs
+++ src/PresentationLayer/UserControls/AdminSubControls/IProgramInfoFormControl.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PresentationLayer.UserControls.AdminSubControls
 {
     internal interface IProgramInfoFormControl
     {
+        TextBox AccessProgramId             { get; }
+        TextBox AccessProgramName           { get; }
+        TextBox AccessDepartmentId          { get; } 
+        TextBox AccessDepartmentName        { get; }
+        IconButton AccessSubmitAddButton    { get; }
+        IconButton AccessSubmitUpdateButton { get; }
 
+        event EventHandler OnControlLoad; 
+        event EventHandler CloseButtonClicked;
+        event EventHandler SubmitAddButtonClicked;
+        event EventHandler SubmitUpdateButtonClicked;
+        event EventHandler CancelSubmitButtonClicked;
     }
 }
