e9b2e4d975b8c484194d2345e5498d4945e991e0
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-15 17:53:26 +0800

    Implemented subscriber OnProgramControl_Load. FINISHED

---
+++ src/PresentationLayer/Presenters/Admin/ProgramInfoFormPresenter.cs
@@ -0,0 +1,55 @@
+﻿using PresentationLayer.Presenters.Enumerations;
+using PresentationLayer.UserControls.AdminSubControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Presenters.Admin
+{
+    internal class ProgramInfoFormPresenter
+    {
+        public ProgramInfoFormPresenter(IProgramInfoFormControl programControl)
+        {
+            _programControl = programControl;
+
+            _programControl.OnControlLoad += OnProgramControl_Load;
+            _programControl.CloseButtonClicked += CloseButton_Clicked;
+            _programControl.SubmitAddButtonClicked += SubmitAddButton_Clicked;
+            _programControl.SubmitUpdateButtonClicked += SubmitUpdateButton_Clicked;
+            _programControl.CancelSubmitButtonClicked += CancelSubmitButton_Clicked;
+        }
+
+
+        private void CancelSubmitButton_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SubmitUpdateButton_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SubmitAddButton_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CloseButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void OnProgramControl_Load(object sender, EventArgs e)
+        {
+            if (_programControl.AccessFormRequestType == FormRequestType.UPDATE)
+                _programControl.AccessSubmitAddButton.Dispose();
+            else if (_programControl.AccessFormRequestType != FormRequestType.ADD)
+                _programControl.AccessSubmitUpdateButton.Dispose();
+        }
+
+        private IProgramInfoFormControl _programControl;
+    }
+}
