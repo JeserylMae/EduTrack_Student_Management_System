4e9626952420483c05e5100a9b448ae5a0d6a8ee
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-15 17:34:04 +0800

    Created new methods which contains initialization of event susbcribers and elements created. FINISHED

--- src/PresentationLayer/UserControls/AdminSubControls/ProgramInfoFormControl.Config.cs
+++ src/PresentationLayer/UserControls/AdminSubControls/ProgramInfoFormControl.Config.cs
@@ -8,5 +8,29 @@ namespace PresentationLayer.UserControls.AdminSubControls
 {
     partial class ProgramInfoFormControl
     {
+        private void InitializeElementCreated()
+        {
+            if (CloseButton != null)                CloseButtonCreated.TrySetResult(true);
+            if (SubmitAddButton != null)            SubmitAddButtonCreated.TrySetResult(true);
+            if (SubmitUpdateButton != null)         SubmitUpdateButtonCreated.TrySetResult(true);
+            if (CancelSubmitButtonClicked != null)  CancelSubmitButtonCreated.TrySetResult(true);
+        }
+
+        private async Task InitializeEventSubscriber()
+        {
+            await CloseButtonCreated.Task;
+            CloseButton.Click += delegate { CloseButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await SubmitAddButtonCreated.Task;
+            SubmitAddButton.Click += delegate { SubmitAddButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await SubmitUpdateButtonCreated.Task;
+            SubmitUpdateButton.Click += delegate { SubmitUpdateButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            await CancelSubmitButtonCreated.Task;
+            CancelSubmitButton.Click += delegate { CancelSubmitButtonClicked?.Invoke(this, EventArgs.Empty); };
+
+            this.Load += delegate { OnControlLoad?.Invoke(this, EventArgs.Empty); };
+        }
     }
 }
