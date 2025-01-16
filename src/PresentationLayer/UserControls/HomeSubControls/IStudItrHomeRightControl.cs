7a976501e3e3f55f5ab2cdf6a6f48524f641d6ff
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-16 19:03:19 +0800

    Added properties to interface. FINISHED

---
+++ src/PresentationLayer/UserControls/HomeSubControls/IStudItrHomeRightControl.cs
@@ -0,0 +1,36 @@
+﻿using PresentationLayer.Presenters.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.UserControls.HomeSubControls
+{
+    internal interface IStudItrHomeRightControl
+    {
+        void DestroyControl();
+        event EventHandler OnControlLoad;
+
+        AccessType CurrentUserType { get; set; }
+
+        Label AccessYearText                { get; }
+        Label AccessSrCodeText              { get; }
+        Label AccessSemesterText            { get; }
+        Label AccessSectionText             { get; }
+        Label AccessProgramText             { get; }
+        Label AccessStudEmailText           { get; }
+        Label AccessStudContactNumberText   { get; }
+
+        Label AccessGenderText              { get; }
+        Label AccessItrCodeText             { get; }
+        Label AccessAddressText             { get; }
+        Label AccessItrEmailText            { get; }
+        Label AccessBirthDateText           { get; }
+        Label AccessItrContactNumberText    { get; }
+
+        FlowLayoutPanel AccessStudentInfoMainPanel      { get; }
+        FlowLayoutPanel AccessInstructorInfoMainPanel   { get; }
+    }
+}
