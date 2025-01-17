01f88f39343d631db1481ebb3ec671bb9125e8bc
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-17 12:49:03 +0800

    Displayed student's information on Home Page. FINISHED

--- src/PresentationLayer/Presenters/StudItr/StudItrHomeRightPresenter.cs
+++ src/PresentationLayer/Presenters/StudItr/StudItrHomeRightPresenter.cs
@@ -4,6 +4,7 @@ using PresentationLayer.UserControls.HomeSubControls;
 using ServiceLayer.Database;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,12 +22,12 @@ namespace PresentationLayer.Presenters.StudItr
         }
 
 
-        private void OnHomeRightControl_Load(object sender, EventArgs e)
+        private async void OnHomeRightControl_Load(object sender, EventArgs e)
         {
             try
             {
                 if (_rightControl.CurrentUserType == AccessType.STUDENT)
-                    HandleStudentInformation();
+                    await HandleStudentInformation();
                 else if (_rightControl.CurrentUserType == AccessType.INSTRUCTOR)
                     HandleInstructorInformation();
                 else
@@ -47,17 +48,74 @@ namespace PresentationLayer.Presenters.StudItr
             throw new NotImplementedException();
         }
 
-        private async void HandleStudentInformation()
+        private async Task HandleStudentInformation()
         {
-            _rightControl.AccessInstructorInfoMainPanel.Dispose();
+            RemovePanel();
 
-            StudentAcademicInfoServices services = new StudentAcademicInfoServices();
+            var academicInfo = await GetStudentAcademicInformation();
+            var personalInfo = await GetStudentPersonalInformation();
+
+            DisplayValuesToPanel( ref personalInfo, ref academicInfo);
+        }
+
+
+        #region Helpers
+        private async Task<RStudentPersonalInfoModel> GetStudentPersonalInformation()
+        {
+            StudentPersonalInfoServices services = new StudentPersonalInfoServices();
+
+            return await services.GetById(_rightControl.CurrentUserId);
+        }
+
+        private async Task<PStudentAcademicInfoModel<PNameModel>> GetStudentAcademicInformation()
+        {
+            StudentAcademicInfoServices services                = new StudentAcademicInfoServices();
+            PRStudentAcademicInfoParams parameters              = new PRStudentAcademicInfoParams();
             List<PStudentAcademicInfoModel<PNameModel>> student = new List<PStudentAcademicInfoModel<PNameModel>>();
 
-            student = await services.GetAll();
+            parameters.SrCode = _rightControl.CurrentUserId;
 
+            student = await services.GetByParams(parameters);
 
+            student = student.Select(row => row)
+                .OrderByDescending(row => row.AcademicYear)
+                .ThenByDescending(row => row.Section)
+                .ToList();
+
+            return student[0];
         }
+
+        private void DisplayValuesToPanel(ref RStudentPersonalInfoModel personalInfo, 
+                              ref PStudentAcademicInfoModel<PNameModel> academicInfo)
+        {
+            _rightControl.AccessFullNameLabel.Text = personalInfo.FirstName + " " 
+                                                   + personalInfo.MiddleName + " "       
+                                                   + personalInfo.LastName;
+
+            _rightControl.AccessSrCodeText.Text             = academicInfo.SrCode;
+            _rightControl.AccessYearText.Text               = academicInfo.YearLevel;
+            _rightControl.AccessSemesterText.Text           = academicInfo.Semester;
+            _rightControl.AccessSectionText.Text            = academicInfo.Section;
+            _rightControl.AccessProgramText.Text            = academicInfo.Program;
+            _rightControl.AccessStudEmailText.Text          = personalInfo.EmailAddress;
+            _rightControl.AccessStudContactNumberText.Text  = personalInfo.ContactNumber;
+        }
+
+        private void RemovePanel()
+        {
+            if (_rightControl.CurrentUserType == AccessType.STUDENT)
+            {
+                _rightControl.AccessMainInfoPanel.Controls.Remove(_rightControl.AccessInstructorInfoMainPanel);
+                _rightControl.AccessInstructorInfoMainPanel.Dispose();
+            }
+            else if (_rightControl.CurrentUserType == AccessType.INSTRUCTOR)
+            {
+                _rightControl.AccessMainInfoPanel.Controls.Remove(_rightControl.AccessStudentInfoMainPanel);
+                _rightControl.AccessStudentInfoMainPanel.Dispose();
+            }
+        }
+        #endregion
+
 
         IStudItrHomeRightControl _rightControl;
     }
