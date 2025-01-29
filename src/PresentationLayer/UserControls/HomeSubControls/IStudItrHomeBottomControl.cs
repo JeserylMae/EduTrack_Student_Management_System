07f2d42ca68fe9a17c1c1b04f90b3586373f2b30
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-29 11:45:40 +0800

    Declared properties of Bottom Home Control. FINISHED

---
+++ src/PresentationLayer/UserControls/HomeSubControls/IStudItrHomeBottomControl.cs
@@ -0,0 +1,20 @@
+﻿using PresentationLayer.Presenters.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.UserControls.HomeSubControls
+{
+    internal interface IStudItrHomeBottomControl
+    {
+        event EventHandler GradeButtonClicked;
+        event EventHandler AttendanceButtonClicked;
+        event EventHandler StudentInfoButtonClicked;
+
+        AccessType AccessCurrentUser { get; set; }
+
+        void DestroyControl();
+    }
+}
