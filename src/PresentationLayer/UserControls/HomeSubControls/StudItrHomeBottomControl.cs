a52c3b89c7962da450f78d27f69b8094fe1ec2ab
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-01-29 11:46:37 +0800

    Defined the properties of Bottom Home Control. FINISHED

--- src/PresentationLayer/UserControls/HomeSubControls/StudItrHomeBottomControl.cs
+++ src/PresentationLayer/UserControls/HomeSubControls/StudItrHomeBottomControl.cs
@@ -1,15 +1,35 @@
 ﻿
+using PresentationLayer.Presenters.Enumerations;
+using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PresentationLayer.UserControls.HomeSubControls
 {
-    public partial class StudItrHomeBottomControl : UserControl
+    public partial class StudItrHomeBottomControl : UserControl, IStudItrHomeBottomControl
     {
         public StudItrHomeBottomControl()
         {
+            GradeButtonCreated       = new TaskCompletionSource<bool>();
+            AttendanceButtonCreated  = new TaskCompletionSource<bool>();
+            StudentInfoButtonCreated = new TaskCompletionSource<bool>();
+
             InitializeComponent();
+            InitializeElementsCreated();
+            _ = InitializeEventSubscriber();
         }
 
+        public AccessType AccessCurrentUser { get; set; }
+
+        public event EventHandler GradeButtonClicked;
+        public event EventHandler AttendanceButtonClicked;
+        public event EventHandler StudentInfoButtonClicked;
+
         public void DestroyControl() { this.Dispose(); }
+
+
+        private TaskCompletionSource<bool> GradeButtonCreated;
+        private TaskCompletionSource<bool> AttendanceButtonCreated;
+        private TaskCompletionSource<bool> StudentInfoButtonCreated;
     }
 }
